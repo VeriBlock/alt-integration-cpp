e2fb32fcf54290ad07e07dd546b2cfd7c940a8a4
Philip Pristupa <PhilipGeneralMail@gmail.com>
2020-01-10 15:15:34 +0200

    clang format

--- include/veriblock/base58.h
+++ include/veriblock/base58.h
@@ -1,13 +1,12 @@
 #ifndef _BASE58_H
 #define _BASE58_H
 
-#include <vector>
 #include <string>
+#include <vector>
 
-namespace Veriblock
-{
-    std::string          base58_encode(const void* buf, size_t size);
-    std::vector<uint8_t> base58_decode(const std::string& str);
-}
+namespace Veriblock {
+std::string base58_encode(const void *buf, size_t size);
+std::vector<uint8_t> base58_decode(const std::string &str);
+} // namespace Veriblock
 
-#endif 
+#endif
