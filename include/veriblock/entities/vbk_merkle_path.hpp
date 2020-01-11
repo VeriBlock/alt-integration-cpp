f39e5899b88dd5857e0c0fa4fa955b6b0c60fd18
Bohdan Vanieiev <warchantua@gmail.com>
2020-01-11 13:48:34 +0200

    Add VbkMerklePath

---
+++ include/veriblock/entities/vbk_merkle_path.hpp
@@ -0,0 +1,37 @@
+#ifndef ALT_INTEGRATION_INCLUDE_VERIBLOCK_ENTITIES_VBK_MERKLE_PATH_HPP_
+#define ALT_INTEGRATION_INCLUDE_VERIBLOCK_ENTITIES_VBK_MERKLE_PATH_HPP_
+
+#include <cstdint>
+
+#include "veriblock/entities/hashes.hpp"
+#include "veriblock/entities/merkle_path.hpp"
+#include "veriblock/serde.hpp"
+
+namespace VeriBlock {
+
+struct VbkMerklePath {
+  int32_t treeIndex{};
+  std::vector<Sha256Hash> layers{};
+  Sha256Hash subject{};
+  int32_t index{};
+
+  static VbkMerklePath fromRaw(ReadStream& stream) {
+    VbkMerklePath mp;
+    mp.treeIndex = readSingleBEValue<int32_t>(stream);
+    mp.index = readSingleBEValue<int32_t>(stream);
+    mp.subject =
+        readSingleByteLenValue(stream, SHA256_HASH_SIZE, SHA256_HASH_SIZE);
+
+    mp.layers = readArrayOf<Sha256Hash>(
+        stream, 0, MAX_LAYER_COUNT_MERKLE, [](ReadStream& stream) {
+          return readSingleByteLenValue(
+              stream, SHA256_HASH_SIZE, SHA256_HASH_SIZE);
+        });
+
+    return mp;
+  }
+};
+
+}  // namespace VeriBlock
+
+#endif  // ALT_INTEGRATION_INCLUDE_VERIBLOCK_ENTITIES_VBK_MERKLE_PATH_HPP_
