9a87adf4ad62263295247c28381ec97c32784569
AharonNavi <57946416+AharonNavi@users.noreply.github.com>
2020-01-16 14:18:49 +0200

    implement sha2 (#13)
    * implement sha2
    
    * add stdint to sha2
    
    * make sha2 as windows buildable
    
    * delete macros
    
    * add stdint to sha2
    
    * Fixes
    
    * Remove sha256_reset from the interfacd
    
    * Fix build
    
    Co-authored-by: Bohdan <bogdan@soramitsu.co.jp>

---
+++ include/veriblock/sha256.h
@@ -0,0 +1,78 @@
+/**
+ * \file sha2.h
+ *
+ *  Copyright (C) 2006-2010, Brainspark B.V.
+ *
+ *  This file is part of PolarSSL (http://www.polarssl.org)
+ *  Lead Maintainer: Paul Bakker <polarssl_maintainer at polarssl.org>
+ *
+ *  All rights reserved.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+#ifndef _SHA2_H
+#define _SHA2_H
+
+#include <stdint.h>
+
+#define SHA256_HASH_SIZE 32
+
+namespace VeriBlock {
+
+/**
+ * \brief          SHA-256 context structure
+ */
+typedef struct {
+  uint32_t total[2];  /*!< number of bytes processed  */
+  uint32_t state[8];  /*!< intermediate digest state  */
+  uint8_t buffer[64]; /*!< data block being processed */
+
+  uint8_t ipad[64]; /*!< HMAC: inner padding        */
+  uint8_t opad[64]; /*!< HMAC: outer padding        */
+  int is224;        /*!< 0 => SHA-256, else SHA-224 */
+} sha256_context;
+
+/**
+ * Initialize sha256 context
+ * @param ctx
+ */
+void sha256_init(sha256_context *ctx);
+
+/**
+ * Add bytes to context
+ * @param ctx context
+ * @param input input bytes
+ * @param ilen length of input bytes
+ */
+void sha256_update(sha256_context *ctx, const uint8_t *input, uint32_t ilen);
+
+/**
+ * Finalize hash calculation. Hash will be written to \p out.
+ * @param ctx context
+ * @param out preallocated output of size 32 bytes
+ */
+void sha256_finish(sha256_context *ctx, uint8_t *out);
+
+/**
+ * All-in-one sha256 hash calculation.
+ * @param out preallocated buffer of 32 bytes
+ * @param buf input buffer
+ * @param nsize size of input buffer
+ */
+void sha256(uint8_t *out, const uint8_t *buf, uint32_t nsize);
+
+}  // namespace VeriBlock
+
+#endif /* sha2.h */
---
+++ test/sha256_test.cpp
@@ -0,0 +1,61 @@
+#include <gtest/gtest.h>
+#include <veriblock/sha256.h>
+
+#include <util/literals.hpp>
+#include <vector>
+
+struct TestCase {
+  std::vector<uint8_t> binData;
+  std::string baseData;
+};
+
+class Sha256Test : public testing::TestWithParam<TestCase> {};
+
+//  test data
+static std::vector<TestCase> g_Cases = {
+    {
+        "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"_unhex,
+        "",
+    },
+
+    {
+        "b5d4045c3f466fa91fe2cc6abe79232a1a57cdf104f7a26e716e0a1e2789df78"_unhex,
+        "ABC",
+    },
+
+    {
+        "b4d5e56e929ba4cda349e9274e3603d0be246b82016bca20f363963c5f2d6845"_unhex,
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+    },
+    {"248d6a61d20638b8e5c026930c3e6039a33ce45964ff2167f6ecedd419db06c1"_unhex,
+     "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq"}};
+
+TEST_P(Sha256Test, OneShot) {
+  auto tc = GetParam();
+  std::vector<uint8_t> result(32, 0);
+  VeriBlock::sha256(result.data(),
+                    (const uint8_t*)(tc.baseData.data()),
+                    (uint32_t)tc.baseData.size());
+  EXPECT_EQ(result, tc.binData);
+}
+
+TEST_P(Sha256Test, IUF) {
+  auto tc = GetParam();
+  std::vector<uint8_t> result(32, 0);
+
+  VeriBlock::sha256_context ctx{};
+  VeriBlock::sha256_init(&ctx);
+
+  // hash of the whole array should equal to the hash of the same string, added
+  // byte by byte through sha256_update
+  for (auto c : tc.baseData) {
+    std::vector<uint8_t> v{(uint8_t)c};
+    VeriBlock::sha256_update(&ctx, v.data(), 1);
+  }
+  VeriBlock::sha256_finish(&ctx, result.data());
+  EXPECT_EQ(result, tc.binData);
+}
+
+INSTANTIATE_TEST_SUITE_P(Sha256Regression,
+                         Sha256Test,
+                         testing::ValuesIn(g_Cases));
