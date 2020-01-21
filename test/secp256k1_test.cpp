84daced163f0e564f136fe14e6275647dfcaa96a
blackyblack <blacky@mail.ru>
2020-01-21 16:14:23 +0200

    secp256k1 sign test

--- test/secp256k1_test.cpp
+++ test/secp256k1_test.cpp
@@ -5,6 +5,8 @@
 #include <vector>
 
 #include "util/literals.hpp"
+#include "veriblock/slice.hpp"
+#include "veriblock/strutil.hpp"
 
 auto defaultPrivateKey =
     "aa4eca67cf7573eaedab283fb5f7c638ec0a9e99583a27288a07e5533b4d6d75"_unhex;
@@ -51,4 +53,30 @@ TEST(SECP256K1, VerifyInvalid) {
   secp256k1_context_destroy(ctx);
 
   EXPECT_EQ(ret, 0);
+}
+
+TEST(SECP256K1, Sign) {
+  secp256k1_context* ctx = secp256k1_context_create(SECP256K1_CONTEXT_SIGN);
+
+  secp256k1_pubkey pubkey;
+  secp256k1_ec_pubkey_create(ctx, &pubkey, defaultPrivateKey.data());
+
+  secp256k1_ecdsa_signature signature;
+  int ret = secp256k1_ecdsa_sign(ctx,
+                       &signature,
+                       defaultMessage.data(),
+                       defaultPrivateKey.data(),
+                       NULL,
+                       NULL);
+
+  EXPECT_EQ(ret, 1);
+
+  unsigned char sig[64]{};
+  secp256k1_ecdsa_signature_serialize_compact(ctx, sig, &signature);
+
+  VeriBlock::Slice<uint8_t> signatureSlice(sig, 64);
+  auto signatureHex = VeriBlock::HexStr(signatureSlice);
+  auto defaultSignatureHex = VeriBlock::HexStr(defaultSignatureCompact);
+
+  EXPECT_EQ(signatureHex, defaultSignatureHex);
 }
