2f07108db6022684a8312f8ca9287c01225110b1
Bohdan <warchantua@gmail.com>
2020-01-23 14:29:50 +0200

    Fix leak; add -Werror; add sanitizers to cmake and CI (#22)
    * Fix leak; add -Werror; add sanitizers to cmake and CI
    
    * Fix

--- test/secp256k1_test.cpp
+++ test/secp256k1_test.cpp
@@ -8,12 +8,12 @@
 #include "veriblock/slice.hpp"
 #include "veriblock/strutil.hpp"
 
-auto defaultPrivateKey =
+static auto defaultPrivateKey =
     "aa4eca67cf7573eaedab283fb5f7c638ec0a9e99583a27288a07e5533b4d6d75"_unhex;
-auto defaultMessage =
+static auto defaultMessage =
     "591b075a0e88b3d4449848ead2f69ff39b145747912c1690457455dd1c90ac64"_unhex;
 
-auto defaultSignatureCompact =
+static auto defaultSignatureCompact =
     "870170004b938c1362e2a329a44ebbb1e4d24f43428466fcc978f4c40d835a52"
     "731ca13658c176a67d8466b5fb46225299985cf1e95b52c42d93205b943f47a3"_unhex;
 
@@ -22,7 +22,8 @@ TEST(SECP256K1, Verify) {
                                                     SECP256K1_CONTEXT_VERIFY);
 
   secp256k1_pubkey pubkey;
-  secp256k1_ec_pubkey_create(ctx, &pubkey, defaultPrivateKey.data());
+  int res = secp256k1_ec_pubkey_create(ctx, &pubkey, defaultPrivateKey.data());
+  EXPECT_EQ(res, 1);
 
   secp256k1_ecdsa_signature signature;
   secp256k1_ecdsa_signature_parse_compact(
@@ -40,7 +41,8 @@ TEST(SECP256K1, VerifyInvalid) {
                                                     SECP256K1_CONTEXT_VERIFY);
 
   secp256k1_pubkey pubkey;
-  secp256k1_ec_pubkey_create(ctx, &pubkey, defaultPrivateKey.data());
+  int res = secp256k1_ec_pubkey_create(ctx, &pubkey, defaultPrivateKey.data());
+  EXPECT_EQ(res, 1);
 
   auto signatureCopy = std::vector<uint8_t>(defaultSignatureCompact);
   signatureCopy[0] = 0xAA;
@@ -59,7 +61,8 @@ TEST(SECP256K1, Sign) {
   secp256k1_context* ctx = secp256k1_context_create(SECP256K1_CONTEXT_SIGN);
 
   secp256k1_pubkey pubkey;
-  secp256k1_ec_pubkey_create(ctx, &pubkey, defaultPrivateKey.data());
+  int res = secp256k1_ec_pubkey_create(ctx, &pubkey, defaultPrivateKey.data());
+  EXPECT_EQ(res, 1);
 
   secp256k1_ecdsa_signature signature;
   int ret = secp256k1_ecdsa_sign(ctx,
@@ -73,6 +76,7 @@ TEST(SECP256K1, Sign) {
 
   unsigned char sig[64]{};
   secp256k1_ecdsa_signature_serialize_compact(ctx, sig, &signature);
+  secp256k1_context_destroy(ctx);
 
   VeriBlock::Slice<uint8_t> signatureSlice(sig, 64);
   auto signatureHex = VeriBlock::HexStr(signatureSlice);
