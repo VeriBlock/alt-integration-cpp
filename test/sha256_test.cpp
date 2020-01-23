2fbf56d3fda50126571d7db775ae1d1b4b0190c1
Alexey Poghilenkov <alex_pozhilenkov@adoriasoft.com>
2020-01-24 08:34:47 +0900

    fix

--- test/sha256_test.cpp
+++ test/sha256_test.cpp
@@ -10,16 +10,16 @@
 
 using namespace VeriBlock;
 
-struct TestCase {
+struct TestCase1 {
   std::vector<uint8_t> data;
   std::vector<uint8_t> hashBytes;
   std::vector<uint8_t> hashTwiceBytes;
 };
 
-class Sha256Test : public testing::TestWithParam<TestCase> {};
+class Sha256Test1 : public testing::TestWithParam<TestCase1> {};
 
 //  test data
-static std::vector<TestCase> g_Cases = {
+static std::vector<TestCase1> sha256_cases = {
     {""_v,
      "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"_unhex,
      "5df6e0e2761359d30a8275058e299fcc0381534545f55cf43e41983f5d4c9456"_unhex},
@@ -36,19 +36,19 @@ static std::vector<TestCase> g_Cases = {
      "248d6a61d20638b8e5c026930c3e6039a33ce45964ff2167f6ecedd419db06c1"_unhex,
      "0cffe17f68954dac3a84fb1458bd5ec99209449749b2b308b7cb55812f9563af"_unhex}};
 
-TEST_P(Sha256Test, OneShot) {
+TEST_P(Sha256Test1, OneShot) {
   auto tc = GetParam();
   auto result = VeriBlock::sha256(tc.data);
   EXPECT_EQ(result, tc.hashBytes);
 }
 
-TEST_P(Sha256Test, HashTwice) {
+TEST_P(Sha256Test1, HashTwice) {
   auto tc = GetParam();
   auto result = VeriBlock::sha256twice(tc.data);
   EXPECT_EQ(result, tc.hashTwiceBytes);
 }
 
-TEST_P(Sha256Test, IUF) {
+TEST_P(Sha256Test1, IUF) {
   auto tc = GetParam();
   std::vector<uint8_t> result(SHA256_HASH_SIZE, 0);
 
@@ -66,8 +66,8 @@ TEST_P(Sha256Test, IUF) {
 }
 
 INSTANTIATE_TEST_SUITE_P(Sha256Regression,
-                         Sha256Test,
-                         testing::ValuesIn(g_Cases));
+                         Sha256Test1,
+                         testing::ValuesIn(sha256_cases));
 
 TEST(Sha256Test, uint256_compareTest) {
   VeriBlock::uint256 hash1(
@@ -94,53 +94,46 @@ TEST(Sha256Test, uint256_compareTest) {
   ASSERT_TRUE(hash1 == hash2);
 }
 
-TEST(Sha256Test, uint256_decodeBits) {
-  uint256 target = decodeBits(0x04800000);
+struct TestCase2 {
+  uint32_t bits;
+  uint256 target;
+};
 
-  EXPECT_EQ(
-      target,
-      uint256(
-          "0000000000000000000000000000000000000000000000000000000000000000"_unhex));
+class DecodeBitsTest : public testing::TestWithParam<TestCase2> {};
 
-  target = decodeBits(0x01123456);
+static std::vector<TestCase2> decodeBits_cases = {
+    {0x04800000,
+     uint256(
+         "0000000000000000000000000000000000000000000000000000000000000000"_unhex)},
+    {0x01123456,
+     uint256(
+         "0000000000000000000000000000000000000000000000000000000000000012"_unhex)},
+    {0x02123456,
+     uint256(
+         "0000000000000000000000000000000000000000000000000000000000001234"_unhex)},
+    {0x03123456,
+     uint256(
+         "0000000000000000000000000000000000000000000000000000000000123456"_unhex)},
+    {0x04123456,
+     uint256(
+         "0000000000000000000000000000000000000000000000000000000012345600"_unhex)},
+    {0x05009234,
+     uint256(
+         "0000000000000000000000000000000000000000000000000000000012345600"_unhex)},
+    {0x20123456,
+     uint256(
+         "1234560000000000000000000000000000000000000000000000000000000000"_unhex)}
 
-  EXPECT_EQ(
-      target,
-      uint256(
-          "0000000000000000000000000000000000000000000000000000000000000012"_unhex));
+};
 
-  target = decodeBits(0x02123456);
+TEST_P(DecodeBitsTest, uint256_decodeBits) {
+  auto value = GetParam();
 
-  EXPECT_EQ(
-      target,
-      uint256(
-          "0000000000000000000000000000000000000000000000000000000000001234"_unhex));
+  uint256 target = decodeBits(value.bits);
 
-  target = decodeBits(0x03123456);
-
-  EXPECT_EQ(
-      target,
-      uint256(
-          "0000000000000000000000000000000000000000000000000000000000123456"_unhex));
-
-  target = decodeBits(0x04123456);
-
-  EXPECT_EQ(
-      target,
-      uint256(
-          "0000000000000000000000000000000000000000000000000000000012345600"_unhex));
-
-  target = decodeBits(0x05009234);
-
-  EXPECT_EQ(
-      target,
-      uint256(
-          "0000000000000000000000000000000000000000000000000000000092340000"_unhex));
-
-  target = decodeBits(0x20123456);
-
-  EXPECT_EQ(
-      target,
-      uint256(
-          "1234560000000000000000000000000000000000000000000000000000000000"_unhex));
+  EXPECT_EQ(target, value.target);
 }
+
+INSTANTIATE_TEST_SUITE_P(decodeBitsRegression,
+                         DecodeBitsTest,
+                         testing::ValuesIn(decodeBits_cases));
