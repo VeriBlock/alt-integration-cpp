eefbd8ce36eec6a384b506f5b53e40a13d1ed822
Bohdan Vanieiev <warchantua@gmail.com>
2020-02-12 13:26:43 +0200

    Autogenerate

---
+++ tools/filetoheader.cpp
@@ -0,0 +1,40 @@
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+template <typename InStream, typename OutStream>
+void write(InStream& in, OutStream& out, const std::string& var) {
+  out << "namespace generated {" << std::endl;
+  out << "extern const char " << var << "[] = {" << std::endl;
+  std::string line;
+  while (std::getline(in, line)) {
+    for (char c : line) {
+      out << (int)c << ", ";
+    }
+    out << (int)'\n' << ", " << std::endl;
+  }
+  out << 0 << std::endl;     // null terminator
+  out << "};" << std::endl;  // end of array
+  out << "};" << std::endl;  // end of namespace
+}
+
+int main(int argc, char** argv) {
+  if (argc != 4) {
+    std::cout << R"(  Usage:
+   stringtobytes variable_name infile outfile
+)";
+    return 1;
+  }
+
+  std::string var = argv[1];
+  std::string in = argv[2];
+  std::string out = argv[3];
+  std::fstream fout(out, std::fstream::out);
+  std::fstream fin(in, std::fstream::in);
+
+  write(fin, fout, var);
+
+  return 0;
+}
