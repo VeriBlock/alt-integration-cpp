eb9ce28df503b8dd686e7ee6e0f2945acc7c345b
Alexey Poghilenkov <alex_pozhilenkov@adoriasoft.com>
2020-03-19 20:05:01 +0200

    add AltTree struct. First version

--- include/veriblock/blockchain/alt_chain_params.hpp
+++ include/veriblock/blockchain/alt_chain_params.hpp
@@ -3,6 +3,8 @@
 
 #include <vector>
 
+#include "veriblock/entities/altblock.hpp"
+
 namespace altintegration {
 
 struct AltChainParams {
@@ -14,6 +16,10 @@ struct AltChainParams {
 
   virtual std::vector<uint32_t> getForkResolutionLookUpTable() const noexcept {
     return {100, 100, 95, 89, 80, 69, 56, 40, 21};
+  }
+
+  virtual AltBlock getGenesisBlock() const noexcept {
+    return {{1, 2, 3}, {4, 5, 6}, 0, 0};
   }
 };
 
---
+++ test/blockchain/alt_blockchain_test.cpp
@@ -0,0 +1,105 @@
+#include <gtest/gtest.h>
+
+#include <algorithm>
+
+#include "veriblock/blockchain/alt_block_tree.hpp"
+#include "veriblock/blockchain/alt_chain_params.hpp"
+
+using namespace altintegration;
+
+struct AltTreeTest : public AltTree {
+  AltChainParams config;
+
+  ValidationState state;
+
+  AltTreeTest() : AltTree(config) { bootstrapWithGenesis(state); }
+
+  std::vector<AltBlock> getTips() {
+    std::vector<AltBlock> res;
+    for (const auto& tip : chainTips_) {
+      res.push_back(tip->header);
+    }
+    return res;
+  }
+};
+
+TEST_F(AltTreeTest, acceptBlock_test) {
+  Payloads temp;
+
+  AltBlock block1;
+  block1.hash = {1, 2, 5};
+  block1.previousBlock = config.getGenesisBlock().hash;
+  block1.height = config.getGenesisBlock().height + 1;
+  block1.timestamp = 0;
+
+  acceptBlock(block1, temp, state);
+
+  EXPECT_TRUE(state.IsValid());
+  EXPECT_TRUE(std::find(this->getTips().begin(),
+                        this->getTips().end(),
+                        block1) != this->getTips().end());
+
+  AltBlock block2;
+  block1.hash = {2, 2, 2};
+  block1.previousBlock = config.getGenesisBlock().hash;
+  block1.height = config.getGenesisBlock().height + 1;
+  block1.timestamp = 0;
+
+  acceptBlock(block2, temp, state);
+
+  EXPECT_TRUE(state.IsValid());
+  EXPECT_TRUE(std::find(this->getTips().begin(),
+                        this->getTips().end(),
+                        block1) != this->getTips().end());
+
+  EXPECT_TRUE(std::find(this->getTips().begin(),
+                        this->getTips().end(),
+                        block2) != this->getTips().end());
+
+  AltBlock block3;
+  block1.hash = {3, 2, 3};
+  block1.previousBlock = block1.hash;
+  block1.height = block1.height + 1;
+  block1.timestamp = 0;
+
+  acceptBlock(block3, temp, state);
+
+  EXPECT_TRUE(state.IsValid());
+  EXPECT_TRUE(std::find(this->getTips().begin(),
+                        this->getTips().end(),
+                        block1) == this->getTips().end());
+
+  EXPECT_TRUE(std::find(this->getTips().begin(),
+                        this->getTips().end(),
+                        block2) != this->getTips().end());
+
+  EXPECT_TRUE(std::find(this->getTips().begin(),
+                        this->getTips().end(),
+                        block3) != this->getTips().end());
+
+  AltBlock block4;
+  block1.hash = {4, 2, 5};
+  block1.previousBlock = block2.hash;
+  block1.height = block2.height + 1;
+  block1.timestamp = 0;
+
+  acceptBlock(block4, temp, state);
+
+  EXPECT_TRUE(state.IsValid());
+  EXPECT_TRUE(std::find(this->getTips().begin(),
+                        this->getTips().end(),
+                        block1) == this->getTips().end());
+
+  EXPECT_TRUE(state.IsValid());
+  EXPECT_TRUE(std::find(this->getTips().begin(),
+                        this->getTips().end(),
+                        block2) == this->getTips().end());
+
+  EXPECT_TRUE(std::find(this->getTips().begin(),
+                        this->getTips().end(),
+                        block3) != this->getTips().end());
+
+  EXPECT_TRUE(std::find(this->getTips().begin(),
+                        this->getTips().end(),
+                        block4) != this->getTips().end());
+}
