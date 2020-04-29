e39fcb8ca4eed7522cc05d497080f875b111871e
Bohdan Vanieiev <warchantua@gmail.com>
2020-04-29 13:14:46 +0300

    Fix clang-tidy; disable -Werror for clang-tidy (temporary)

--- cmake/clang-tidy.cmake
+++ cmake/clang-tidy.cmake
@@ -8,5 +8,10 @@ else()
     message(STATUS "clang-tidy has been found: ${CLANG_TIDY_BIN}")
 endif()
 
-set(CMAKE_C_CLANG_TIDY   ${CLANG_TIDY_BIN})
-set(CMAKE_CXX_CLANG_TIDY ${CLANG_TIDY_BIN})
+
+set(__TIDY_CMD
+        ${CLANG_TIDY_BIN}
+        -p=${CMAKE_BINARY_DIR}
+        )
+set(CMAKE_C_CLANG_TIDY   ${__TIDY_CMD})
+set(CMAKE_CXX_CLANG_TIDY ${__TIDY_CMD})
