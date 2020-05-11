a9ea3e11f00eec2a0607b1e3dd502b88858f184f
Bohdan Vanieiev <warchantua@gmail.com>
2020-05-11 13:31:48 +0300

    Update name

--- pypopminer/pypopminer/__init__.py
+++ pypopminer/pypopminer/__init__.py
@@ -4,4 +4,4 @@
 # Distributed under the MIT software license, see the accompanying
 # file LICENSE or http://www.opensource.org/licenses/mit-license.php.
 
-from ._pypopminer import *
+from .pypopminer import *
