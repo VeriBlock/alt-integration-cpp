de33f149dece02a67ca0f565ec195f7666cee84d
Alexey Poghilenkov <alex_pozhilenkov@adoriasoft.com>
2020-05-19 18:23:42 +0300

    3

--- src/blockchain/alt_chain_params_serializable.cpp
+++ src/blockchain/alt_chain_params_serializable.cpp
@@ -3,25 +3,9 @@
 namespace altintegration {
 
 void PopRewardsCurveParams::toRaw(WriteStream& stream) const {
-  std::string temp;
-
-  temp = std::to_string(this->startOfSlope());
-  writeSingleBEValue(stream, temp.size());
-  for (const char& el : temp) {
-    stream.writeBE<char>(el);
-  }
-
-  temp = std::to_string(this->slopeNormal());
-  writeSingleBEValue(stream, temp.size());
-  for (const char& el : temp) {
-    stream.writeBE<char>(el);
-  }
-
-  temp = std::to_string(this->slopeKeystone());
-  writeSingleBEValue(stream, temp.size());
-  for (const char& el : temp) {
-    stream.writeBE<char>(el);
-  }
+  writeDouble(stream, this->startOfSlope());
+  writeDouble(stream, this->slopeNormal());
+  writeDouble(stream, this->slopeKeystone());
 }
 
 std::vector<uint8_t> PopRewardsCurveParams::toRaw() const {
@@ -34,9 +18,9 @@ PopRewardsCurveParamsSerializable PopRewardsCurveParamsSerializable::fromRaw(
     ReadStream& stream) {
   PopRewardsCurveParamsSerializable param;
 
-  param.startOfSlope_ = std::atof(readString(stream).data());
-  param.slopeNormal_ = std::atof(readString(stream).data());
-  param.slopeKeystone_ = std::atof(readString(stream).data());
+  param.startOfSlope_ = readDouble(stream);
+  param.slopeNormal_ = readDouble(stream);
+  param.slopeKeystone_ = readDouble(stream);
 
   return param;
 }
@@ -45,6 +29,94 @@ PopRewardsCurveParamsSerializable PopRewardsCurveParamsSerializable::fromRaw(
     const std::vector<uint8_t>& bytes) {
   ReadStream stream(bytes);
   return fromRaw(stream);
+}
+
+void PopRewardsParams::toRaw(WriteStream& stream) const {
+  stream.writeBE<uint32_t>(this->keystoneRound());
+  stream.writeBE<uint32_t>(this->payoutRounds());
+  stream.writeBE<uint32_t>(this->flatScoreRound());
+  stream.writeBE<bool>(this->flatScoreRoundUse());
+
+  writeSingleBEValue(stream, this->roundRatios().size());
+  for (const auto& el : this->roundRatios()) {
+    writeDouble(stream, el);
+  }
+
+  writeDouble(stream, this->maxScoreThresholdNormal());
+  writeDouble(stream, this->maxScoreThresholdKeystone());
+
+  stream.writeBE<uint32_t>(this->difficultyAveragingInterval());
+  stream.writeBE<uint32_t>(this->rewardSettlementInterval());
+
+  writeSingleBEValue(stream, this->relativeScoreLookupTable().size());
+  for (const auto& el : this->relativeScoreLookupTable()) {
+    writeDouble(stream, el);
+  }
+
+  this->getCurveParams().toRaw(stream);
+}
+
+std::vector<uint8_t> PopRewardsParams::toRaw() const {
+  WriteStream stream;
+  toRaw(stream);
+  return stream.data();
+}
+
+PopRewardsParamsSerializable PopRewardsParamsSerializable::fromRaw(
+    ReadStream& stream) {
+  PopRewardsParamsSerializable param;
+
+  param.keystoneRound_ = stream.readBE<uint32_t>();
+  param.payoutRounds_ = stream.readBE<uint32_t>();
+  param.flatScoreRound_ = stream.readBE<uint32_t>();
+  param.flatScoreRoundUse_ = stream.readBE<bool>();
+
+  param.roundRatios_ =
+      readArrayOf<double>(stream, 0, MAX_CONTEXT_COUNT, readDouble);
+
+  param.maxScoreThresholdNormal_ = readDouble(stream);
+  param.maxScoreThresholdKeystone_ = readDouble(stream);
+  param.difficultyAveragingInterval_ = stream.readBE<uint32_t>();
+  param.rewardSettlementInterval_ = stream.readBE<uint32_t>();
+
+  param.relativeScoreLookupTable =
+      readArrayOf<double>(stream, 0, MAX_CONTEXT_COUNT, readDouble);
+
+  param.curveParams = std::make_shared<PopRewardsCurveParamsSerializable>(
+      PopRewardsCurveParamsSerializable::fromRaw(stream));
+
+  return param;
+}
+
+PopRewardsParamsSerializable PopRewardsParamsSerializable::fromRaw(
+    const std::vector<uint8_t>& bytes) {
+  ReadStream stream(bytes);
+  return fromRaw(stream);
+}
+
+std::vector<uint8_t> AltChainParams::toRaw() const {
+  WriteStream stream;
+  toRaw(stream);
+  return stream.data();
+}
+
+void AltChainParams::toRaw(WriteStream& stream) const {
+  stream.writeBE<uint32_t>(this->getKeystoneInterval());
+  stream.writeBE<uint32_t>(this->getFinalityDelay());
+
+  writeSingleBEValue(stream, this->getForkResolutionLookUpTable().size());
+  for (const auto& val : this->getForkResolutionLookUpTable()) {
+    stream.writeBE<uint32_t>(val);
+  }
+
+  stream.writeBE<int32_t>(this->getEndorsementSettlementInterval());
+  stream.writeBE<uint32_t>(this->getMaxPopDataPerBlock());
+  stream.writeBE<uint32_t>(this->getMaxPopDataWeight());
+  stream.writeBE<uint32_t>(this->getSuperMaxPopDataWeight());
+  stream.writeBE<uint32_t>(this->getIdentifier());
+
+  this->getBootstrapBlock().toVbkEncoding(stream);
+  this->getRewardParams().toRaw(stream);
 }
 
 }  // namespace altintegration
