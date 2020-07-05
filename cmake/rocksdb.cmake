6ae8ff21399c7a2aecdcc1f0c3eee0afd469d23e
Bohdan Vanieiev <warchantua@gmail.com>
2020-07-05 13:07:54 +0300

    Install rocksdb

--- cmake/rocksdb.cmake
+++ cmake/rocksdb.cmake
@@ -1,32 +1,27 @@
-# Download and unpack RocksDB at configure time
-configure_file(CMakeLists-rocksdb.txt.in rocksdb-download/CMakeLists.txt)
-execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
-        RESULT_VARIABLE result
-        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/rocksdb-download )
-if(result)
-    message(FATAL_ERROR "CMake step for RocksDB failed: ${result}")
-endif()
-execute_process(COMMAND ${CMAKE_COMMAND} --build .
-        RESULT_VARIABLE result
-        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/rocksdb-download )
-if(result)
-    message(FATAL_ERROR "Build step for RocksDB failed: ${result}")
-endif()
-
 set(WITH_GFLAGS 0 CACHE BOOL "build with gflags")
 set(WITH_TESTS OFF CACHE BOOL "build with tests")
 set(WITH_BENCHMARK_TOOLS OFF CACHE BOOL "build with benchmarks")
 set(WITH_TOOLS OFF CACHE BOOL "build with tools")
+set(WITH_EXAMPLES OFF CACHE BOOL "build examples")
 set(FAIL_ON_WARNINGS OFF CACHE BOOL "fail on warnings")
 set(ROCKSDB_BUILD_SHARED FALSE CACHE BOOL "build shared library alongside with static")
 set(WITH_MD_LIBRARY OFF CACHE BOOL "build with MD runtime")
 set(PORTABLE ON CACHE BOOL "build portable")
 set(CMAKE_DISABLE_FIND_PACKAGE_NUMA TRUE CACHE BOOL "disable find NUMA package")
 
-# Add RocksDB directly to our build.
-add_subdirectory(${CMAKE_CURRENT_BINARY_DIR}/rocksdb-src
-        ${CMAKE_CURRENT_BINARY_DIR}/rocksdb-build
-        EXCLUDE_FROM_ALL)
+FetchContent_Declare(
+        rocksdb
+        GIT_REPOSITORY https://github.com/facebook/rocksdb.git
+        GIT_TAG        v6.10.2
+)
+
+FetchContent_GetProperties(rocksdb)
+if(NOT rocksdb_POPULATED)
+    message(STATUS "Downloading rocksdb...")
+    FetchContent_Populate(rocksdb)
+    add_subdirectory(${rocksdb_SOURCE_DIR} ${rocksdb_BINARY_DIR})
+endif()
+
 
 if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "^(AppleClang|Clang|GNU)$")
     target_add_flag(rocksdb -Wno-format-y2k)
