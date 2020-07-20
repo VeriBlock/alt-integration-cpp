e95238541e515c402bc8a31c08250be63daed2a8
Bohdan <warchantua@gmail.com>
2020-07-20 19:12:06 +0300

    Revert "VbkBlockTree: optimize addPayloads; plug the validation hole (#296)" (#303)
    This reverts commit dea010815ca4aeaae5b369ecc13c2645515f3d25.

--- include/veriblock/blockchain/btc_block_addon.hpp
+++ include/veriblock/blockchain/btc_block_addon.hpp
@@ -13,59 +13,24 @@
 namespace altintegration {
 
 struct BtcBlockAddon {
-  using ref_height_t = int32_t;
-
   //! (memory only) total amount of work in the chain up to and including this
   //! block
   ArithUint256 chainWork = 0;
 
-  void setIsBootstrap(bool isBootstrap) {
-    if (isBootstrap) {
-      // pretend this block is referenced by the genesis block of the SI chain
-      addRef(0);
-    } else {
-      VBK_ASSERT(false && "not supported");
-    }
-  }
+  uint32_t getRefCounter() const { return _refCounter; }
 
-  uint32_t refCount() const { return (uint32_t)refs.size(); }
-
-  const std::vector<ref_height_t>& getRefs() const { return refs; }
-
-  void addRef(ref_height_t referencedAtHeight) {
-    refs.push_back(referencedAtHeight);
+  void incRefCounter() {
+    _refCounter++;
     setDirty();
   }
 
-  void removeRef(ref_height_t referencedAtHeight) {
-    auto ref_it = find(refs.begin(), refs.end(), referencedAtHeight);
-    VBK_ASSERT(ref_it != refs.end() &&
-               "state corruption: tried removing a nonexistent reference to a "
-               "BTC block");
-    refs.erase(ref_it);
+  void decRefCounter() {
+    _refCounter--;
     setDirty();
-  }
-
-  void toRaw(WriteStream& w) const {
-    // save only refs
-    writeArrayOf<ref_height_t>(
-        w, refs, [](WriteStream& stream, ref_height_t value) {
-          stream.writeBE<ref_height_t>(value);
-        });
-  }
-
-  void initAddonFromRaw(ReadStream& r) {
-    refs = readArrayOf<ref_height_t>(
-        r, [](ReadStream& stream) { return stream.readBE<ref_height_t>(); });
   }
 
   bool operator==(const BtcBlockAddon& o) const {
-    // comparing reference counts does not seem like a good idea
-    // as the only situation where they would be different is
-    // comparing blocks across different trees eg mock miner vs
-    // the test tree and in this situation the references and counts
-    // are likely to differ
-    bool a = true;  // refs == o.refs;
+    bool a = _refCounter == o._refCounter;
     bool b = chainWork == o.chainWork;
     return a && b;
   }
@@ -74,26 +39,21 @@ struct BtcBlockAddon {
     return fmt::format("chainwork={}", chainWork.toHex());
   }
 
+  void toRaw(WriteStream& w) const { w.writeBE<uint32_t>(_refCounter); }
+
  protected:
   //! reference counter for fork resolution
-  // Ideally we would want a sorted collection with cheap addition, deletion and
-  // lookup. In practice, due to VBK/BTC block time ratio(20x) and multiple APMs
-  // running, there's little chance of VTBs shipping non-empty BTC context. The
-  // altchain mempool prioritization algo will realistically pick 1 VTB per VBK
-  // keystone period(20 blocks), providing us with the BTC block mined during
-  // this period. Thus, we can expect to have 1-2 references per block and 2x
-  // that during fork resolution making std::vector the fastest storage option
-  // in this case.
-  // TODO: figure out if this is somehow abusable by spammers/dosers
-
-  std::vector<ref_height_t> refs{};
+  uint32_t _refCounter = 0;
 
   void setDirty();
 
   void setNull() {
-    refs.clear();
+    _refCounter = 0;
     chainWork = 0;
   }
+
+  // not static, on purpose
+  void initAddonFromRaw(ReadStream& r) { _refCounter = r.readBE<uint32_t>(); }
 };
 
 }  // namespace altintegration
