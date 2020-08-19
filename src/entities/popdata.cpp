58d2e7a4ef3f36b3895fdeb6b5242f6ff9620202
Bohdan <warchantua@gmail.com>
2020-08-19 20:06:31 +0300

    Add ATV/VTB/PopData version field (#347)

--- src/entities/popdata.cpp
+++ src/entities/popdata.cpp
@@ -10,24 +10,29 @@
 namespace altintegration {
 
 PopData PopData::fromVbkEncoding(ReadStream& stream) {
-  PopData alt_pop_tx;
-  alt_pop_tx.context = readArrayOf<VbkBlock>(
-      stream,
-      0,
-      MAX_CONTEXT_COUNT_ALT_PUBLICATION,
-      (VbkBlock(*)(ReadStream&))VbkBlock::fromVbkEncoding);
+  PopData pd;
+  pd.version = stream.readBE<uint32_t>();
+  if (pd.version == 1) {
+    pd.context = readArrayOf<VbkBlock>(
+        stream,
+        0,
+        MAX_CONTEXT_COUNT,
+        (VbkBlock(*)(ReadStream&))VbkBlock::fromVbkEncoding);
 
-  alt_pop_tx.atvs = readArrayOf<ATV>(stream,
-                                     0,
-                                     MAX_CONTEXT_COUNT_ALT_PUBLICATION,
-                                     (ATV(*)(ReadStream&))ATV::fromVbkEncoding);
+    pd.atvs = readArrayOf<ATV>(stream,
+                               0,
+                               MAX_CONTEXT_COUNT_ALT_PUBLICATION,
+                               (ATV(*)(ReadStream&))ATV::fromVbkEncoding);
 
-  alt_pop_tx.vtbs = readArrayOf<VTB>(stream,
-                                     0,
-                                     MAX_CONTEXT_COUNT_ALT_PUBLICATION,
-                                     (VTB(*)(ReadStream&))VTB::fromVbkEncoding);
-
-  return alt_pop_tx;
+    pd.vtbs = readArrayOf<VTB>(stream,
+                               0,
+                               MAX_CONTEXT_COUNT_VBK_PUBLICATION,
+                               (VTB(*)(ReadStream&))VTB::fromVbkEncoding);
+  } else {
+    throw std::domain_error(fmt::format(
+        "PopData deserialization version={} is not implemented", pd.version));
+  }
+  return pd;
 }
 
 PopData PopData::fromVbkEncoding(Slice<const uint8_t> raw_bytes) {
@@ -36,19 +41,25 @@ PopData PopData::fromVbkEncoding(Slice<const uint8_t> raw_bytes) {
 }
 
 void PopData::toVbkEncoding(WriteStream& stream) const {
-  writeSingleBEValue(stream, context.size());
-  for (const auto& b : context) {
-    b.toVbkEncoding(stream);
-  }
+  stream.writeBE<uint32_t>(version);
+  if (version == 1) {
+    writeSingleBEValue(stream, context.size());
+    for (const auto& b : context) {
+      b.toVbkEncoding(stream);
+    }
 
-  writeSingleBEValue(stream, atvs.size());
-  for (const auto& atv : atvs) {
-    atv.toVbkEncoding(stream);
-  }
+    writeSingleBEValue(stream, atvs.size());
+    for (const auto& atv : atvs) {
+      atv.toVbkEncoding(stream);
+    }
 
-  writeSingleBEValue(stream, vtbs.size());
-  for (const auto& vtb : vtbs) {
-    vtb.toVbkEncoding(stream);
+    writeSingleBEValue(stream, vtbs.size());
+    for (const auto& vtb : vtbs) {
+      vtb.toVbkEncoding(stream);
+    }
+  } else {
+    VBK_ASSERT_MSG(
+        false, "PopData serialization version=%d is not implemented", version);
   }
 }
 
@@ -56,11 +67,6 @@ std::vector<uint8_t> PopData::toVbkEncoding() const {
   WriteStream stream;
   toVbkEncoding(stream);
   return stream.data();
-}
-
-PopData::id_t PopData::getHash() const {
-  auto bytes = toVbkEncoding();
-  return sha256(bytes);
 }
 
 }  // namespace altintegration
