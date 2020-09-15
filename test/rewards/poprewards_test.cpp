d63e71a9c13b0a46837f9079333c1ee5f3a621a3
blackyblack <blacky@mail.ru>
2020-09-15 15:29:43 +0200

    Rewards cache negative test

--- test/rewards/poprewards_test.cpp
+++ test/rewards/poprewards_test.cpp
@@ -15,6 +15,7 @@ struct RewardsTestFixture : public ::testing::Test, public PopTestFixture {
   BlockIndex<VbkBlock>* vbktip;
   std::vector<AltBlock> altchain;
   std::shared_ptr<PopRewardsCalculator> sampleCalculator;
+  std::shared_ptr<PopRewards> sampleRewards;
 
   ValidationState state;
 
@@ -26,6 +27,7 @@ struct RewardsTestFixture : public ::testing::Test, public PopTestFixture {
     mineAltBlocks(10, altchain);
 
     sampleCalculator = std::make_shared<PopRewardsCalculator>(altparam);
+    sampleRewards = std::make_shared<PopRewards>(altparam, alttree.vbk());
 
     EXPECT_EQ(altchain.size(), 11);
     EXPECT_EQ(altchain.at(altchain.size() - 1).height, 10);
@@ -156,4 +158,92 @@ TEST_F(RewardsTestFixture, hugeFlatReward_test) {
       ((double)payouts.begin()->second) / PopRewardsBigDecimal::decimals,
       1.07,
       0.1);
+}
+
+TEST_F(RewardsTestFixture, basicCacheReward_test) {
+  /*mineAltBlocks(altparam.getRewardParams().difficultyAveragingInterval() + 1, altchain);
+  AltBlock endorsedBlock = altchain.back();
+  endorseLastBlock(10);
+
+  auto payouts = alttree.getPopPayout(altchain.back().getHash());
+  ASSERT_TRUE(payouts.size());
+
+  auto payoutBlockRound =
+      sampleCalculator->getRoundForBlockNumber(endorsedBlock.height);
+  ASSERT_EQ(payouts.size(), 1);
+
+  auto chain2 = altchain;
+  chain2.resize(altchain.size() - 5);
+  auto containingBlock = generateNextBlock(*chain2.rbegin());
+  chain2.push_back(containingBlock);
+  auto
+  PopData altPayloads3 = generateAltPayloads({tx}, vbkTip1->getHash());*/
+
+  std::vector<AltBlock> chain = {altparam.getBootstrapBlock()};
+
+  // mine 100 blocks
+  mineAltBlocks(100, chain, true);
+
+  AltBlock endorsedBlock = chain[95];
+  auto* endorsedIndex = alttree.getBlockIndex(endorsedBlock.getHash());
+
+  VbkTx tx = popminer->createVbkTxEndorsingAltBlock(
+      generatePublicationData(endorsedBlock));
+  AltBlock containingBlock = generateNextBlock(*chain.rbegin());
+  chain.push_back(containingBlock);
+
+  PopData altPayloads1 =
+      generateAltPayloads({tx}, vbkparam.getGenesisBlock().getHash());
+
+  // mine 5 VBK blocks
+  auto* vbkTip = popminer->mineVbkBlocks(5);
+
+  EXPECT_TRUE(alttree.acceptBlockHeader(containingBlock, state));
+  EXPECT_TRUE(AddPayloads(containingBlock.getHash(), altPayloads1));
+  EXPECT_TRUE(alttree.setState(containingBlock.getHash(), state));
+  EXPECT_TRUE(state.IsValid());
+
+  EXPECT_EQ(chain.back().height, 101);
+  EXPECT_EQ(sampleRewards->scoreFromEndorsements(*endorsedIndex),
+            1.0);
+  mineAltBlocks(altparam.getPopPayoutDelay() - (101 - 95), chain, true);
+
+  auto payouts = alttree.getPopPayout(chain.back().getHash());
+  ASSERT_EQ(payouts.size(), 1);
+
+  auto payoutsUncached = sampleRewards->calculatePayouts(*endorsedIndex);
+  ASSERT_EQ(payoutsUncached.size(), 1);
+  ASSERT_EQ(payoutsUncached.begin()->second, payouts.begin()->second);
+
+  // reset state of the cmp_ in the altTree
+  // generate new fork with the new altPayloads
+  vbkTip = alttree.vbk().getBestChain().tip();
+  auto chain2 = chain;
+  chain2.resize(chain.size() - 5);
+  containingBlock = generateNextBlock(*chain2.rbegin());
+  chain2.push_back(containingBlock);
+
+  VbkTx tx2 = popminer->createVbkTxEndorsingAltBlock(
+      generatePublicationData(endorsedBlock));
+  PopData altPayloads3 = generateAltPayloads({tx2}, vbkTip->getHash());
+  vbkTip = popminer->mineVbkBlocks(1);
+
+  EXPECT_TRUE(alttree.acceptBlockHeader(containingBlock, state));
+  EXPECT_TRUE(AddPayloads(containingBlock.getHash(), altPayloads3));
+  EXPECT_TRUE(alttree.setState(containingBlock.getHash(), state))
+      << state.toString();
+  EXPECT_TRUE(state.IsValid());
+
+  // after reorg the score has changed
+  EXPECT_FALSE(sampleRewards->scoreFromEndorsements(*endorsedIndex) == 1.0);
+
+  mineAltBlocks(4, chain2, true);
+  payouts = alttree.getPopPayout(chain2.back().getHash());
+  ASSERT_EQ(payouts.size(), 1);
+  payoutsUncached = sampleRewards->calculatePayouts(*endorsedIndex);
+
+  // we didn't call getPopPayout() for each subsequent block therefore
+  // our cache is bad now
+  ASSERT_EQ(payoutsUncached.size(), 1);
+  ASSERT_NE(payoutsUncached.begin()->second, payouts.begin()->second);
 }
