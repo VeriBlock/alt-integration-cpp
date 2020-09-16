fb591ee4974faec3725ea81870cf42c806716e38
blackyblack <blacky@mail.ru>
2020-09-16 14:10:40 +0200

    Cleanup

--- src/blockchain/alt_block_tree.cpp
+++ src/blockchain/alt_block_tree.cpp
@@ -373,14 +373,6 @@ int AltBlockTree::comparePopScore(const AltBlock::hash_t& A,
   // compare current active chain to other chain
   int result = cmp_.comparePopScore(*this, *right, state);
   if (result < 0) {
-    // invalidate rewards cache if necessary
-    //uint32_t invalidBlocks = std::numeric_limits<uint32_t>::max();
-
-    /*const auto* fork = activeChain_.findFork(right);
-    if (fork != nullptr) {
-      invalidBlocks = activeChain_.tip()->getHeight() - fork->getHeight();
-    }*/
-    //rewards_.eraseCacheHistory(invalidBlocks);
     rewards_.invalidateCache();
 
     // other chain is better, and we already changed 'cmp' state to winner, so
