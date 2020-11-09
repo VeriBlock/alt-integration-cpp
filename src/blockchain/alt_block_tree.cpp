6f5d0d116cb0fc5e801931c9207ffab520f686f1
Bohdan <warchantua@gmail.com>
2020-11-09 20:10:41 +0200

    Mark expensive asserts as DEBUG to avoid them running in RELEASE mode (#420)
    * Mark expensive asserts as DEBUG to avoid them running in RELEASE mode
    
    * Add _DEBUG
    
    * FIx
    
    * Fix
    
    * Fix
    
    * Fix

--- src/blockchain/alt_block_tree.cpp
+++ src/blockchain/alt_block_tree.cpp
@@ -168,11 +168,9 @@ void AltBlockTree::setPayloads(index_t& index, const PopData& payloads) {
   VBK_ASSERT_MSG(index.pprev,
                  "Adding payloads to a bootstrap block is not allowed");
 
-  // FIXME: it should be impossible to construct popData without running this
-  // check
   ValidationState state;
-  VBK_ASSERT_MSG(
-      checkPopData(payloads, state),
+  VBK_ASSERT_MSG_DEBUG(
+      checkPopDataForDuplicates(payloads, state),
       "attempted to add statelessly invalid payloads to block %s: %s",
       index.toPrettyString(),
       state.toString());
@@ -193,13 +191,12 @@ bool AltBlockTree::connectBlock(index_t& index, ValidationState& state) {
   VBK_ASSERT_MSG(!index.hasFlags(BLOCK_APPLIED),
                  "state corruption: block %s is applied",
                  index.toPrettyString());
-
   VBK_ASSERT_MSG(index.pprev->isConnected(),
                  "the previous block of block %s must be connected",
                  index.toPrettyString());
-  VBK_ASSERT_MSG(index.allDescendantsUnconnected(),
-                 "a descendant of block %s is connected",
-                 index.toPrettyString());
+  VBK_ASSERT_MSG_DEBUG(index.allDescendantsUnconnected(),
+                       "a descendant of block %s is connected",
+                       index.toPrettyString());
 
   bool success = index.raiseValidity(BLOCK_CONNECTED);
   VBK_ASSERT(success);
@@ -219,23 +216,20 @@ bool AltBlockTree::connectBlock(index_t& index, ValidationState& state) {
 
   if (index.isValid()) {
     onBlockConnected.emit(index);
+    // connect the descendants
+    for (auto* successor : index.pnext) {
+      if (successor->isValidUpTo(BLOCK_HAS_PAYLOADS)) {
+        ValidationState dummy;
+        connectBlock(*successor, dummy);
+      }
+    }
   } else {
     onInvalidBlockConnected.emit(index, state);
   };
 
-  // connect the descendants
-  for (auto* successor : index.pnext) {
-    if (successor->isValidUpTo(BLOCK_HAS_PAYLOADS)) {
-      ValidationState dummy;
-      connectBlock(*successor, dummy);
-    }
-  }
-
   return index.isValid();
 }
 
-// in !StrictAddPayloadsOrdering mode, payloads can be added to any block, which
-// may trigger incorrect AltTree behavior in certain cases
 bool AltBlockTree::addPayloads(index_t& index,
                                PopData& payloads,
                                ValidationState& state) {
@@ -249,6 +243,10 @@ bool AltBlockTree::addPayloads(index_t& index,
   if (!index.isValid()) {
     return state.Invalid(block_t::name() + "-bad-chain",
                          "Containing block has been marked as invalid");
+  }
+
+  if (!checkPopDataForDuplicates(payloads, state)) {
+    return state.Invalid("popdata");
   }
 
   setPayloads(index, payloads);
