5619259345177429aad69c401d9b2275aed71697
Mr-Leshiy <leshiy12345678@gmail.com>
2020-12-17 15:17:28 +0200

    add pop_reward entity

---
+++ include/veriblock/entities/pop_rewards.hpp
@@ -0,0 +1,41 @@
+// Copyright (c) 2019-2020 Xenios SEZC
+// https://www.veriblock.org
+// Distributed under the MIT software license, see the accompanying
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef ALT_INTEGRATION_INCLUDE_VERIBLOCK_ENTITIES_POP_REWARDS_HPP_
+#define ALT_INTEGRATION_INCLUDE_VERIBLOCK_ENTITIES_POP_REWARDS_HPP_
+
+#include <cstdint>
+#include <map>
+#include <vector>
+
+#include "veriblock/serde.hpp"
+#include "veriblock/validation_state.hpp"
+
+namespace altintegration {
+
+/**
+ * @struct PopRewards
+ * @ingroup entities
+ */
+struct PopRewards {
+  std::map<std::vector<uint8_t>, int64_t> payout;
+
+  /**
+   * Convert PopRewards to data stream using PublicationData byte
+   * format
+   * @param stream data stream to write into
+   */
+  void toVbkEncoding(WriteStream& stream) const;
+
+  size_t estimateSize() const;
+};
+
+bool DeserializeFromVbkEncoding(ReadStream& stream,
+                                PopRewards& out,
+                                ValidationState& state);
+
+}  // namespace altintegration
+
+#endif
---
+++ src/entities/pop_rewards.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2019-2020 Xenios SEZC
+// https://www.veriblock.org
+// Distributed under the MIT software license, see the accompanying
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+
+#include "veriblock/entities/pop_rewards.hpp"
+
+namespace altintegration {
+
+void PopRewards::toVbkEncoding(WriteStream& stream) const { (void)stream; }
+}  // namespace altintegration
