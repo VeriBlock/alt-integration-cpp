2986d022812783ebc45ef3e664ad298dd072c59f
Nikolai Viktorov <nk.viktorov@gmail.com>
2020-12-31 00:48:21 +0700

    Add nolint to mock_miner_2

--- src/mock_miner_2.cpp
+++ src/mock_miner_2.cpp
@@ -13,10 +13,12 @@
 
 namespace altintegration {
 
+// NOLINTNEXTLINE(cert-err58-cpp)
 static auto defaultPrivateKeyVbk = ParseHex(
     "303e020100301006072a8648ce3d020106052b8104000a0427302502010104203abf83fa47"
     "0423d4788a760ef6b7aae1dacf98784b0646057a0adca24e522acb");
 
+// NOLINTNEXTLINE(cert-err58-cpp)
 static auto defaultPublicKeyVbk = ParseHex(
     "3056301006072a8648ce3d020106052b8104000a034200042fca63a20cb5208c2a55ff5099"
     "ca1966b7f52e687600784d1de062c1dd9c8a5fe55b2ba5d906c703d37cbd02ecd9c97a8061"
