8aa1901921d0848b83c74205d77a03ad686f8fa8
Mr-Leshiy <leshiy12345678@gmail.com>
2021-01-11 16:19:01 +0200

    add StorageIterator

---
+++ include/veriblock/storage/storage_iterator.hpp
@@ -0,0 +1,48 @@
+// Copyright (c) 2019-2020 Xenios SEZC
+// https://www.veriblock.org
+// Distributed under the MIT software license, see the accompanying
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef VERIBLOCK_POP_CPP_STORAGE_ITERATOR_HPP
+#define VERIBLOCK_POP_CPP_STORAGE_ITERATOR_HPP
+
+#include <stdint.h>
+
+#include <vector>
+
+/**
+ * @defgroup interfaces Interfaces to be implemented
+ * These interfaces must be implemented by Altchain developers for integration
+ * of POP protocol.
+ */
+
+namespace altintegration {
+
+/**
+ * @struct StorageIterator
+ *
+ * An abstraction over on-disk storage iterator.
+ *
+ * veriblock-pop-cpp does not dictate how to iterate over the storage. Altchain
+ * must create derived class.
+ *
+ * @ingroup interfaces
+ */
+
+struct StorageIterator {
+  virtual ~StorageIterator() = default;
+
+  virtual bool next() = 0;
+
+  virtual std::vector<uint8_t> key() const = 0;
+
+  virtual std::vector<uint8_t> value() const = 0;
+
+  virtual bool valid() const = 0;
+
+  virtual bool seek() = 0;
+};
+
+}  // namespace altintegration
+
+#endif
