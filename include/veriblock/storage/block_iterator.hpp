4681a02f07042588210de6ed72298c698d5b75f0
Alex Pozhylenkov <alex_pozhilenkov@adoriasoft.com>
2021-01-13 18:41:10 +0200

    add LoadAllTrees function (#513)

---
+++ include/veriblock/storage/block_iterator.hpp
@@ -0,0 +1,67 @@
+// Copyright (c) 2019-2020 Xenios SEZC
+// https://www.veriblock.org
+// Distributed under the MIT software license, see the accompanying
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef VERIBLOCK_POP_CPP_STORAGE_ITERATOR_HPP
+#define VERIBLOCK_POP_CPP_STORAGE_ITERATOR_HPP
+
+#include <stdint.h>
+
+#include <vector>
+
+#include "veriblock/blockchain/block_index.hpp"
+
+namespace altintegration {
+
+/**
+ * @struct BlockIterator
+ *
+ * An abstraction over on-disk storage block iterator.
+ *
+ * veriblock-pop-cpp does not dictate how to iterate blocks over the storage.
+ * Altchain must create derived class.
+ *
+ * @ingroup interfaces
+ */
+
+template <typename BlockT>
+struct BlockIterator {
+  using hash_t = typename BlockT::hash_t;
+
+  virtual ~BlockIterator() = default;
+
+  /**
+   * Iterate to the next block.
+   */
+  virtual void next() = 0;
+
+  /**
+   * Return the current block value
+   * @param[out] current block, altintegration::BlockIndex
+   * @return true if we can successfully return value, false otherwise
+   */
+  virtual bool value(BlockIndex<BlockT>& out) const = 0;
+
+  /**
+   * Return the current block hash key
+   * @param[out] current block hash
+   * @return true if we can successfully return key, false otherwise
+   */
+  virtual bool key(hash_t& out) const = 0;
+
+  /**
+   * Return if valid iterator or not
+   * @return true if the iterator is valid, false otherwise
+   */
+  virtual bool valid() const = 0;
+
+  /**
+   * Set iterator to the start of the collection
+   */
+  virtual void seek_start() = 0;
+};
+
+}  // namespace altintegration
+
+#endif
--- include/veriblock/storage/util.hpp
+++ include/veriblock/storage/util.hpp
@@ -10,6 +10,7 @@
 #include <veriblock/blockchain/alt_block_tree.hpp>
 #include <veriblock/logger.hpp>
 #include <veriblock/storage/block_batch_adaptor.hpp>
+#include <veriblock/storage/block_provider.hpp>
 #include <veriblock/validation_state.hpp>
 
 namespace altintegration {
@@ -46,7 +47,6 @@ bool LoadTree(BlockTreeT& tree,
   return tree.loadTip(tiphash, state);
 }
 
-//! Save blocks and tip to batch
 template <typename BlockTreeT>
 void SaveTree(BlockTreeT& tree, BlockBatchAdaptor& batch) {
   for (auto& block : tree.getBlocks()) {
@@ -63,6 +63,12 @@ void SaveTree(BlockTreeT& tree, BlockBatchAdaptor& batch) {
 struct AltBlockTree;
 
 void SaveAllTrees(AltBlockTree& tree, BlockBatchAdaptor& batch);
+
+bool LoadAllTrees(AltBlockTree& tree,
+                  BlockProvider<BtcBlock>& btc_provider,
+                  BlockProvider<VbkBlock>& vbk_provider,
+                  BlockProvider<AltBlock>& alt_provider,
+                  ValidationState& state);
 
 }  // namespace altintegration
 
