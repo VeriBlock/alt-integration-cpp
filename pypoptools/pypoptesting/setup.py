52b8105567acb3eba42058e268b1c11982b2e0c5
Aleksa Milosevic <amilosevic@animus.rs>
2021-01-25 12:42:15 +0100

    Added setup.py

---
+++ pypoptools/pypoptesting/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="pypoptesting",
+    version="0.0.1",
+    author="VeriBlock",
+    author_email="info@VeriBlock.com",
+    description="Testing package for PoP protocol",
+    url="https://github.com/VeriBlock/alt-integration-cpp/tree/feature/pypoptesting/pypoptools/pypoptesting",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
