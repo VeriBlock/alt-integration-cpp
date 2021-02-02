ab13ef85d1f0b36658815466dc2b1b5cc2247630
Nikolai Viktorov <nk.viktorov@gmail.com>
2021-02-02 22:48:29 +0700

    ALT-402 Fix poptesting tests (#554)
    * Fix dict mapping on entities
    
    * Fix tests and clean up test framework
    
    * Add ConnectAndSync test
    
    * ALT-402: Review fixes
    
    * ALT-402: Get rid of setup_network and use only setup_nodes

--- pypoptools/pypoptesting/tests/pop_mempool_sync.py
+++ pypoptools/pypoptesting/tests/pop_mempool_sync.py
@@ -2,28 +2,38 @@
 Test with multiple nodes, and multiple PoP endorsements, checking to make sure nodes stay in sync.
 """
 from ..framework.test_framework import PopIntegrationTestFramework
-from ..framework.util import endorse_block, get_best_block
+from ..framework.pop_util import endorse_block, mine_until_pop_enabled
+from ..framework.sync_util import (
+    start_all_and_wait, connect_all,
+    sync_all, sync_pop_mempools, wait_for_rpc_availability
+)
 
 
 class PopMempoolSync(PopIntegrationTestFramework):
     def set_test_params(self):
         self.num_nodes = 2
 
-    def skip_test_if_missing_module(self):
-        self.skip_if_no_pypopminer()
+    def setup_nodes(self):
+        start_all_and_wait(self.nodes)
+        mine_until_pop_enabled(self.nodes[0])
+        connect_all(self.nodes)
+        sync_all(self.nodes)
 
     def run_test(self):
         from pypoptools.pypopminer import MockMiner
         apm = MockMiner()
-        addr0 = self.nodes[0].getnewaddress()
 
         self.log.info("node0 endorses block 5")
         self.nodes[0].generate(nblocks=10)
 
-        tip_height = get_best_block(self.nodes[0]).height
-        atv_id = endorse_block(self.nodes[0], apm, tip_height - 5, addr0)
+        tip_height = self.nodes[0].getblockcount()
+        atv_id = endorse_block(self.nodes[0], apm, tip_height - 5)
 
-        self.sync_pop_mempools(self.nodes, timeout=20)
+        rawpopmempool1 = self.nodes[1].getrawpopmempool()
+        assert atv_id not in rawpopmempool1.atvs
+        self.log.info("node1 does not contain atv1 in its pop mempool after restart")
+
+        sync_pop_mempools(self.nodes, timeout=20)
         self.log.info("nodes[0,1] have syncd pop mempools")
 
         rawpopmempool1 = self.nodes[1].getrawpopmempool()
@@ -31,6 +41,7 @@ class PopMempoolSync(PopIntegrationTestFramework):
         self.log.info("node1 contains atv1 in its pop mempool")
 
         self.nodes[1].restart()
+        wait_for_rpc_availability(self.nodes[1])
         self.log.info("node1 has been restarted")
         rawpopmempool1 = self.nodes[1].getrawpopmempool()
         assert atv_id not in rawpopmempool1.atvs
@@ -39,7 +50,7 @@ class PopMempoolSync(PopIntegrationTestFramework):
         self.nodes[0].connect(self.nodes[1])
         self.log.info("node1 connect to node0")
 
-        self.sync_pop_mempools(self.nodes, timeout=20)
+        sync_pop_mempools(self.nodes, timeout=20)
         self.log.info("nodes[0,1] have syncd pop mempools")
 
         rawpopmempool1 = self.nodes[1].getrawpopmempool()
--- pypoptools/pypoptesting/tests/pop_sync.py
+++ pypoptools/pypoptesting/tests/pop_sync.py
@@ -2,43 +2,45 @@
 Test with multiple nodes, and multiple PoP endorsements, checking to make sure nodes stay in sync.
 """
 from ..framework.test_framework import PopIntegrationTestFramework
-from ..framework.util import endorse_block, wait_for_block_height, KEYSTONE_INTERVAL
+from ..framework.pop_util import endorse_block, mine_until_pop_enabled
+from ..framework.sync_util import start_all_and_wait, connect_all, sync_all, wait_for_block_height
 
 
 class PopSync(PopIntegrationTestFramework):
     def set_test_params(self):
         self.num_nodes = 3
 
-    def skip_test_if_missing_module(self):
-        self.skip_if_no_pypopminer()
+    def setup_nodes(self):
+        start_all_and_wait(self.nodes)
+        mine_until_pop_enabled(self.nodes[0])
+        connect_all(self.nodes)
+        sync_all(self.nodes)
 
     def run_test(self):
         from pypoptools.pypopminer import MockMiner
         apm = MockMiner()
 
-        addr0 = self.nodes[0].getnewaddress()
-        addr1 = self.nodes[1].getnewaddress()
-        addr2 = self.nodes[2].getnewaddress()
+        keystone_interval = self.nodes[0].getpopparams().keystoneInterval
 
         for height in range(self.nodes[0].getblockcount(), 52):
             self.nodes[0].generate(nblocks=1)
             # endorse every block
             wait_for_block_height(self.nodes[2], height)
-            self.log.info("node2 endorsing block {} by miner {}".format(height, addr2))
-            node2_atv_id = endorse_block(self.nodes[2], apm, height, addr2)
+            self.log.info("node2 endorsing block {}".format(height))
+            node2_atv_id = endorse_block(self.nodes[2], apm, height)
 
             # endorse each keystone
-            if height % KEYSTONE_INTERVAL == 0:
+            if height % keystone_interval == 0:
                 wait_for_block_height(self.nodes[0], height)
-                self.log.info("node0 endorsing block {} by miner {}".format(height, addr0))
-                node0_atv_id = endorse_block(self.nodes[0], apm, height, addr0)
+                self.log.info("node0 endorsing block {}".format(height))
+                node0_atv_id = endorse_block(self.nodes[0], apm, height)
 
                 wait_for_block_height(self.nodes[1], height)
-                self.log.info("node1 endorsing block {} by miner {}".format(height, addr1))
-                node1_atv_id = endorse_block(self.nodes[1], apm, height, addr1)
+                self.log.info("node1 endorsing block {}".format(height))
+                node1_atv_id = endorse_block(self.nodes[1], apm, height)
 
                 # wait until node[1] gets relayed pop tx
-                self.sync_all(self.nodes, timeout=20)
+                sync_all(self.nodes, timeout=20)
                 self.log.info("transactions relayed")
 
                 # mine a block on node[1] with this pop tx
@@ -57,4 +59,5 @@ class PopSync(PopIntegrationTestFramework):
                             "containing block {} does not contain ATV {}".format(containing_block_hash, atv_id)
 
                 # assert that node height matches
-                assert self.nodes[0].getblockcount() == self.nodes[1].getblockcount() == self.nodes[2].getblockcount()
+                assert self.nodes[0].getblockcount() == self.nodes[1].getblockcount()
+                assert self.nodes[0].getblockcount() == self.nodes[2].getblockcount()
