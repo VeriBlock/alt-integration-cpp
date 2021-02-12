1f202cd84e9acd474542f19260725609858b7cfc
Alex Pozhylenkov <alex_pozhilenkov@adoriasoft.com>
2021-02-12 13:23:55 +0200

    Update ValidationState (#578)

--- bindings/go/ffi/validation_state_test.go
+++ bindings/go/ffi/validation_state_test.go
@@ -1,9 +1,31 @@
 package ffi
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestValidationStateFree(t *testing.T) {
 	state := NewValidationState()
 	state.Free()
 	state.Free()
+}
+
+func TestValidationStateBasic(t *testing.T) {
+	assert := assert.New(t)
+
+	state := NewValidationState()
+	defer state.Free()
+
+	assert.NoError(state.Error())
+	assert.Equal(state.IsInvalid(), false)
+	assert.Equal(state.IsValid(), true)
+	assert.Equal(state.GetErrorMessage(), "")
+
+	assert.Equal(state.Invalid("error message", "debug message"), false)
+	assert.Error(state.Error())
+	assert.Equal(state.IsInvalid(), true)
+	assert.Equal(state.IsValid(), false)
+	assert.Equal(state.GetErrorMessage(), "error message, debug message")
 }
