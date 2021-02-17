3d5c659ff09ef6f91645f6619b35709c0a8b2d6c
Bohdan Vanieiev <warchantua@gmail.com>
2021-02-17 15:38:52 +0200

    Fix merkle tree

--- include/veriblock/entities/merkle_tree.hpp
+++ include/veriblock/entities/merkle_tree.hpp
@@ -60,6 +60,8 @@ struct MerkleTree {
 
   hash_t getMerkleRoot() { return instance.finalizeRoot(); }
 
+  const std::vector<hash_t>& getLayers() const { return layers; }
+
  protected:
   void buildTree(std::vector<hash_t> layer) {
     size_t n = layer.size();
@@ -75,7 +77,7 @@ struct MerkleTree {
 
     layer.resize(n % 2 == 0 ? n : n + 1);
     while (n > 1) {
-      if (n % 2) {
+      if ((n % 2) != 0u) {
         layer[n] = layer[n - 1];
         ++n;
       }
@@ -151,8 +153,13 @@ struct BtcMerkleTree : public MerkleTree<BtcMerkleTree, uint256> {
   }
 
   hash_t finalizeRoot() {
-    VBK_ASSERT(!layers.empty());
-    return layers[0][0].reverse();
+    if (layers.empty()) {
+      return hash_t{};
+    }
+
+    auto& root = layers.back();
+    VBK_ASSERT(root.size() == 1);
+    return root[0].reverse();
   }
 };
 
@@ -176,9 +183,12 @@ struct PayloadsMerkleTree
 
   hash_t finalizeRoot() {
     if (this->layers.empty()) {
-      return hash_t();
+      return hash_t{};
     }
-    return this->layers[0][0].reverse();
+
+    auto& root = this->layers.back();
+    VBK_ASSERT(root.size() == 1);
+    return root[0].reverse();
   }
 };
 
