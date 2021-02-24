b7424fd818d0611e16338da13999f22a1dcc4824
Mr-Leshiy <leshiy12345678@gmail.com>
2021-02-24 15:37:01 +0200

    update

---
+++ include/veriblock/value_sorted_map.hpp
@@ -0,0 +1,19 @@
+// Copyright (c) 2019-2020 Xenios SEZC
+// https://www.veriblock.org
+// Distributed under the MIT software license, see the accompanying
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef ALTINTEGRATION_VALUE_SORTED_MAP___
+#define ALTINTEGRATION_VALUE_SORTED_MAP___
+
+#include <algorithm>
+#include <unordered_map>
+
+namespace altintegration {
+
+template <typename T>
+struct ValueSortedMap {};
+
+}  // namespace altintegration
+
+#endif
