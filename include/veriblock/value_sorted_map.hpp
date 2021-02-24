684151d417bed079909d2672fd7acedd092738f5
Mr-Leshiy <leshiy12345678@gmail.com>
2021-02-25 00:20:33 +0200

    update

--- include/veriblock/value_sorted_map.hpp
+++ include/veriblock/value_sorted_map.hpp
@@ -26,24 +26,41 @@ struct ValueSortedMap {
 
     SetCmp(const cmp_t& cmp) : cmp_(cmp) {}
 
-    bool operator()(const pair_t& val1, const pair_t& val2) {
+    bool operator()(const pair_t& val1, const pair_t& val2) const {
       return cmp_(val1.second, val2.second);
     }
 
    private:
-    cmp_t cmp_;
+    cmp_t cmp_{};
   };
 
  public:
+  using iterator_t = typename std::set<pair_t, SetCmp>::iterator;
+
   ~ValueSortedMap() = default;
 
   ValueSortedMap() = default;
 
   ValueSortedMap(const cmp_t& cmp) : set_(SetCmp(cmp)), map_() {}
 
+  iterator_t find(const key_t& key) {
+    auto it = map_.find(key);
+    if (it != map_.end()) {
+      return set_.find(*it);
+    }
+    return set_.end();
+  }
+
+  iterator_t& begin() { return set_.begin(); }
+
+  iterator_t end() { return set_.end(); }
+
   void erase(const key_t& key) {
-    set_.erase(map_[key]);
-    map_.erase(key);
+    auto it = map_.find(key);
+    if (it != map_.end()) {
+      set_.erase(*it);
+      map_.erase(it);
+    }
 
     VBK_ASSERT_MSG(map_.size() == set_.size(),
                    "size of the map and vector incompetible map: %d, vec: %d",
@@ -53,6 +70,9 @@ struct ValueSortedMap {
 
   void insert(const key_t& key, const val_t& value) {
     pair_t pair(key, value);
+
+    auto it = map_.find(key);
+
 
     VBK_ASSERT_MSG(map_.size() == set_.size(),
                    "size of the map and vector incompetible map: %d, vec: %d",
@@ -67,6 +87,24 @@ struct ValueSortedMap {
                    set_.size());
 
     return map_.size();
+  }
+
+  void clear() {
+    VBK_ASSERT_MSG(map_.size() == set_.size(),
+                   "size of the map and vector incompetible map: %d, vec: %d",
+                   map_.size(),
+                   set_.size());
+    set_.clear();
+    map_.clear();
+  }
+
+  bool empty() const {
+    VBK_ASSERT_MSG(map_.size() == set_.size(),
+                   "size of the map and vector incompetible map: %d, vec: %d",
+                   map_.size(),
+                   set_.size());
+
+    return map_.empty();
   }
 
  private:
--- test/value_sorted_map_test.cpp
+++ test/value_sorted_map_test.cpp
@@ -12,7 +12,7 @@
 using namespace altintegration;
 
 struct IntCmp {
-  bool operator()(int a, int b) { return a < b; }
+  bool operator()(int a, int b) const { return a < b; }
 };
 
 TEST(ValueSortedMap, Basic_test) {
@@ -20,4 +20,33 @@ TEST(ValueSortedMap, Basic_test) {
 
   map.insert("hello", 5);
   map.insert("hello 2", 6);
+  map.insert("hello", 10);
+
+  EXPECT_EQ(map.size(), 2);
+  EXPECT_FALSE(map.empty());
+
+  map.erase("hello 1");
+  map.erase("hello");
+
+  EXPECT_EQ(map.size(), 1);
+  EXPECT_FALSE(map.empty());
+
+  map.clear();
+
+  EXPECT_EQ(map.size(), 0);
+  EXPECT_TRUE(map.empty());
+}
+
+TEST(ValueSortedMap, find_test) {
+  ValueSortedMap<std::string, int, IntCmp> map;
+
+  map.insert("hello", 5);
+  map.insert("hello 2", 6);
+  map.insert("hello", 10);
+
+  EXPECT_EQ(map.find("hello 3"), map.end());
+
+  auto it = map.find("hello 2");
+  EXPECT_NE(it, map.end());
+  EXPECT_EQ(it->second, 6);
 }
