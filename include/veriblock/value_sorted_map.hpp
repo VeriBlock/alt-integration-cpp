d529148a9966eb705714494f26808ba8c0cccedc
Alex Pozhylenkov <alex_pozhilenkov@adoriasoft.com>
2021-02-25 12:21:37 +0200

    New ValueSortedMap (#607)

---
+++ include/veriblock/value_sorted_map.hpp
@@ -0,0 +1,121 @@
+// Copyright (c) 2019-2020 Xenios SEZC
+// https://www.veriblock.org
+// Distributed under the MIT software license, see the accompanying
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef ALTINTEGRATION_VALUE_SORTED_MAP___
+#define ALTINTEGRATION_VALUE_SORTED_MAP___
+
+#include <algorithm>
+#include <set>
+#include <unordered_map>
+
+#include "veriblock/assert.hpp"
+
+namespace altintegration {
+
+template <typename key_t, typename val_t, typename cmp_t>
+struct ValueSortedMap {
+  using pair_t = std::pair<key_t, val_t>;
+
+ private:
+  struct SetCmp {
+    ~SetCmp() = default;
+
+    SetCmp() = default;
+
+    SetCmp(const cmp_t& cmp) : cmp_(cmp) {}
+
+    bool operator()(const pair_t& val1, const pair_t& val2) const {
+      return cmp_(val1.second, val2.second);
+    }
+
+   private:
+    cmp_t cmp_{};
+  };
+
+ public:
+  using iterator_t = typename std::set<pair_t, SetCmp>::iterator;
+
+  ~ValueSortedMap() = default;
+
+  ValueSortedMap() = default;
+
+  ValueSortedMap(const cmp_t& cmp) : set_(SetCmp(cmp)), map_() {}
+
+  iterator_t find(const key_t& key) {
+    auto it = map_.find(key);
+    return it != map_.end() ? set_.find(*it) : set_.end();
+  }
+
+  iterator_t begin() const { return set_.begin(); }
+
+  iterator_t end() const { return set_.end(); }
+
+  void erase(const key_t& key) {
+    auto it = map_.find(key);
+    if (it != map_.end()) {
+      set_.erase(*it);
+      map_.erase(it);
+    }
+
+    VBK_ASSERT_MSG(map_.size() == set_.size(),
+                   "size of the map and vector incompetible map: %d, vec: %d",
+                   map_.size(),
+                   set_.size());
+  }
+
+  void insert(const key_t& key, const val_t& value) {
+    pair_t pair(key, value);
+
+    auto it = map_.find(key);
+    if (it != map_.end()) {
+      set_.erase(*it);
+      it->second = value;
+      set_.insert(pair);
+    } else {
+      map_.insert(pair);
+      set_.insert(pair);
+    }
+
+    VBK_ASSERT_MSG(map_.size() == set_.size(),
+                   "size of the map and vector incompetible map: %d, vec: %d",
+                   map_.size(),
+                   set_.size());
+  }
+
+  size_t size() const {
+    VBK_ASSERT_MSG(map_.size() == set_.size(),
+                   "size of the map and vector incompetible map: %d, vec: %d",
+                   map_.size(),
+                   set_.size());
+
+    return map_.size();
+  }
+
+  void clear() {
+    VBK_ASSERT_MSG(map_.size() == set_.size(),
+                   "size of the map and vector incompetible map: %d, vec: %d",
+                   map_.size(),
+                   set_.size());
+    set_.clear();
+    map_.clear();
+  }
+
+  bool empty() const {
+    VBK_ASSERT_MSG(map_.size() == set_.size(),
+                   "size of the map and vector incompetible map: %d, vec: %d",
+                   map_.size(),
+                   set_.size());
+
+    return map_.empty();
+  }
+
+ private:
+  std::multiset<pair_t, SetCmp> set_{};
+  std::unordered_map<key_t, val_t> map_{};
+};
+
+}  // namespace altintegration
+
+#endif
---
+++ test/value_sorted_map_test.cpp
@@ -0,0 +1,80 @@
+// Copyright (c) 2019-2020 Xenios SEZC
+// https://www.veriblock.org
+// Distributed under the MIT software license, see the accompanying
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+
+#include <gtest/gtest.h>
+
+#include <string>
+
+#include "veriblock/value_sorted_map.hpp"
+
+using namespace altintegration;
+
+struct IntCmp {
+  bool operator()(int a, int b) const { return a < b; }
+};
+
+TEST(ValueSortedMap, Basic_test) {
+  ValueSortedMap<std::string, int, IntCmp> map;
+
+  map.insert("hello", 5);
+  map.insert("hello 2", 6);
+  map.insert("hello", 10);
+
+  EXPECT_EQ(map.size(), 2);
+  EXPECT_FALSE(map.empty());
+
+  map.erase("hello 1");
+  map.erase("hello");
+
+  EXPECT_EQ(map.size(), 1);
+  EXPECT_FALSE(map.empty());
+
+  map.clear();
+
+  EXPECT_EQ(map.size(), 0);
+  EXPECT_TRUE(map.empty());
+}
+
+TEST(ValueSortedMap, find_test) {
+  ValueSortedMap<std::string, int, IntCmp> map;
+
+  map.insert("hello", 5);
+  map.insert("hello 2", 6);
+  map.insert("hello", 10);
+
+  EXPECT_EQ(map.find("hello 3"), map.end());
+
+  auto it = map.find("hello 2");
+  EXPECT_NE(it, map.end());
+  EXPECT_EQ(it->second, 6);
+
+  it = map.find("hello");
+  EXPECT_NE(it, map.end());
+  EXPECT_EQ(it->second, 10);
+}
+
+TEST(ValueSortedMap, value_sort_test) {
+  IntCmp cmp;
+  ValueSortedMap<std::string, int, IntCmp> map(cmp);
+  using pair_t = typename ValueSortedMap<std::string, int, IntCmp>::pair_t;
+
+  map.insert("key 1", 4);
+  map.insert("key 4", 2);
+  map.insert("key 5", 6);
+  map.insert("key 6", 1);
+  map.insert("key 7", 12);
+  map.insert("key 8", 4);
+  map.insert("key 9", 6);
+
+  auto it = map.begin();
+
+  EXPECT_EQ(*(it++), pair_t("key 6", 1));
+  EXPECT_EQ(*(it++), pair_t("key 4", 2));
+  EXPECT_EQ(*(it++), pair_t("key 1", 4));
+  EXPECT_EQ(*(it++), pair_t("key 8", 4));
+  EXPECT_EQ(*(it++), pair_t("key 5", 6));
+  EXPECT_EQ(*(it++), pair_t("key 9", 6));
+  EXPECT_EQ(*(it++), pair_t("key 7", 12));
+}
