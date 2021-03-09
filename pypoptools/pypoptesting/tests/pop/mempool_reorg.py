9cb5a971c3b7f9ff3124c5dbf4438a8b6c9a5f0c
Mr-Leshiy <leshiy12345678@gmail.com>
2021-03-09 12:35:59 +0200

    fix

--- pypoptools/pypoptesting/tests/pop/mempool_reorg.py
+++ pypoptools/pypoptesting/tests/pop/mempool_reorg.py
@@ -28,8 +28,8 @@ class PopMempoolReorgTest(PopIntegrationTestFramework):
         vbk_blocks = mine_vbk_blocks(self.nodes[0], apm, vbk_blocks_amount)
 
         # mine a block on node[0] with these vbk blocks
-        node0_tip_hash = self.nodes[0].generate(nblocks=1)[0]
-        node0_tip = self.nodes[0].getblock(node0_tip_hash)
+        self.nodes[0].generate(nblocks=1)
+        node0_tip = self.nodes[0].getbestblock()
 
         assert len(vbk_blocks) == vbk_blocks_amount
         assert len(node0_tip.containingVBKs) == vbk_blocks_amount
@@ -40,8 +40,8 @@ class PopMempoolReorgTest(PopIntegrationTestFramework):
         assert node1_tip.hash != node0_tip.hash
 
         self.log.info("node 1 mine 10 blocks")
-        node1_tip_hash = self.nodes[1].generate(nblocks=10)[9]
-        node1_tip = self.nodes[1].getblock(node1_tip_hash)
+        self.nodes[1].generate(nblocks=10)
+        node1_tip = self.nodes[1].getbestblock()
 
         assert node1_tip == self.nodes[1].getbestblock()
 
@@ -54,8 +54,8 @@ class PopMempoolReorgTest(PopIntegrationTestFramework):
         assert self.nodes[1].getbestblock() == self.nodes[0].getbestblock()
 
         # mine a block on node[1] with these vbk blocks
-        tip_hash = self.nodes[1].generate(nblocks=1)[0]
-        tip = self.nodes[1].getblock(tip_hash)
+        self.nodes[1].generate(nblocks=1)
+        tip = self.nodes[1].getbestblock()
 
         assert len(vbk_blocks) == vbk_blocks_amount
         assert len(tip.containingVBKs) == vbk_blocks_amount
