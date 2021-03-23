a33d5d5e6967baac5c9706259d672d5880ab2a6e
Bohdan Vanieiev <warchantua@gmail.com>
2021-03-23 19:04:58 +0200

    Fix

--- docs/modules.hpp
+++ docs/modules.hpp
@@ -1,9 +1,9 @@
-@defgroup popcontext PopContext
-
-@defgroup mockminer MockMiner
-
-@defgroup pypoptools PyPoptools
-
-@defgroup cbindings C Bindings
-
-@defgroup gobindings Go Bindings
+//! @defgroup popcontext PopContext
+//!
+//! @defgroup mockminer MockMiner
+//!
+//! @defgroup pypoptools PyPoptools
+//!
+//! @defgroup cbindings C Bindings
+//!
+//! @defgroup gobindings Go Bindings
