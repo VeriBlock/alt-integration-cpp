ec261452bb3409e96d55c3154474ad7077d314cb
blackyblack <blackyblack@users.noreply.github.com>
2021-03-29 11:17:03 +0000

    Deploying to docs from @ VeriBlock/alt-integration-cpp@128ce44875468b051f07eea0e9d2f2508fec8a88 🚀

--- search/classes_c.js
+++ search/classes_c.js
@@ -1,12 +1,12 @@
 var searchData=
 [
-  ['payloadsstorage_473',['PayloadsStorage',['../structaltintegration_1_1PayloadsStorage.html',1,'altintegration']]],
-  ['popcontext_474',['PopContext',['../structaltintegration_1_1PopContext.html',1,'altintegration']]],
-  ['popdata_475',['PopData',['../structaltintegration_1_1PopData.html',1,'altintegration']]],
-  ['poppayouts_476',['PopPayouts',['../structaltintegration_1_1PopPayouts.html',1,'altintegration']]],
-  ['poppayoutsparams_477',['PopPayoutsParams',['../structaltintegration_1_1PopPayoutsParams.html',1,'altintegration']]],
-  ['poprewardscalculator_478',['PopRewardsCalculator',['../structaltintegration_1_1PopRewardsCalculator.html',1,'altintegration']]],
-  ['poprewardscurveparams_479',['PopRewardsCurveParams',['../structaltintegration_1_1PopRewardsCurveParams.html',1,'altintegration']]],
-  ['poprewardsparams_480',['PopRewardsParams',['../structaltintegration_1_1PopRewardsParams.html',1,'altintegration']]],
-  ['publicationdata_481',['PublicationData',['../structaltintegration_1_1PublicationData.html',1,'altintegration']]]
+  ['payloadsstorage_474',['PayloadsStorage',['../structaltintegration_1_1PayloadsStorage.html',1,'altintegration']]],
+  ['popcontext_475',['PopContext',['../structaltintegration_1_1PopContext.html',1,'altintegration']]],
+  ['popdata_476',['PopData',['../structaltintegration_1_1PopData.html',1,'altintegration']]],
+  ['poppayouts_477',['PopPayouts',['../structaltintegration_1_1PopPayouts.html',1,'altintegration']]],
+  ['poppayoutsparams_478',['PopPayoutsParams',['../structaltintegration_1_1PopPayoutsParams.html',1,'altintegration']]],
+  ['poprewardscalculator_479',['PopRewardsCalculator',['../structaltintegration_1_1PopRewardsCalculator.html',1,'altintegration']]],
+  ['poprewardscurveparams_480',['PopRewardsCurveParams',['../structaltintegration_1_1PopRewardsCurveParams.html',1,'altintegration']]],
+  ['poprewardsparams_481',['PopRewardsParams',['../structaltintegration_1_1PopRewardsParams.html',1,'altintegration']]],
+  ['publicationdata_482',['PublicationData',['../structaltintegration_1_1PublicationData.html',1,'altintegration']]]
 ];
