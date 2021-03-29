9b6e730677860fe3cf490466abdb379911a283b3
Mr-Leshiy <leshiy12345678@gmail.com>
2021-03-29 17:48:51 +0300

    update

--- include/veriblock/pop/entities/vbkblock.hpp
+++ include/veriblock/pop/entities/vbkblock.hpp
@@ -172,7 +172,7 @@ JsonValue ToJSON(const VbkBlock& b) {
   json::putStringKV(
       obj, "secondPreviousKeystone", HexStr(b.getSecondPreviousKeystone()));
   json::putStringKV(obj, "merkleRoot", HexStr(b.getMerkleRoot()));
-  json::putIntKV(obj, "timestamp", b.getBlockTime());
+  json::putIntKV(obj, "timestamp", b.getTimestamp());
   json::putIntKV(obj, "difficulty", b.getDifficulty());
   json::putIntKV(obj, "nonce", b.getNonce());
   return obj;
