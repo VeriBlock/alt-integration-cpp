a4b1748bde8476c3175eb066fa7360931e02b4cb
blackyblack <blackyblack@users.noreply.github.com>
2021-03-30 12:59:47 +0000

    Deploying to docs from @ VeriBlock/alt-integration-cpp@ab526c5fbfc3f532f58a549c83b01471db895770 🚀

--- dir_0400561d0d5a2d57dc000ed65d436d0c.js
+++ dir_0400561d0d5a2d57dc000ed65d436d0c.js
@@ -4,6 +4,5 @@ var dir_0400561d0d5a2d57dc000ed65d436d0c =
     [ "btcblock.h", "btcblock_8h_source.html", null ],
     [ "entities.h", "entities_8h_source.html", null ],
     [ "serde.h", "serde_8h_source.html", null ],
-    [ "type_helpers.h", "type__helpers_8h_source.html", null ],
-    [ "vbkblock.h", "vbkblock_8h_source.html", null ]
+    [ "type_helpers.h", "type__helpers_8h_source.html", null ]
 ];
