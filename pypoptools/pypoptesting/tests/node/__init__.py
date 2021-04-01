4f1ec49b72898fe51660de6a693b5c2eba312d11
Alex Pozhylenkov <alex_pozhilenkov@adoriasoft.com>
2021-04-01 13:28:03 +0300

    Add NodeBasicSyncTest python test (#670)

--- pypoptools/pypoptesting/tests/node/__init__.py
+++ pypoptools/pypoptesting/tests/node/__init__.py
@@ -1,3 +1,4 @@
 from .restart import NodeRestartTest
 from .start import NodeStartTest
 from .stop import NodeStopTest
+from .basic_sync import NodeBasicSyncTest
--- pypoptools/pypoptesting/tests/node/restart.py
+++ pypoptools/pypoptesting/tests/node/restart.py
@@ -1,4 +1,5 @@
 from ...framework.test_framework import PopIntegrationTestFramework
+from ...framework.sync_util import start_all
 
 
 class NodeRestartTest(PopIntegrationTestFramework):
@@ -6,5 +7,14 @@ class NodeRestartTest(PopIntegrationTestFramework):
         self.num_nodes = 3
 
     def run_test(self):
+        start_all(self.nodes)
+
+        for node in self.nodes:
+            assert node.getbestblock().height == 0
+
         for node in self.nodes:
             node.restart()
+
+        for node in self.nodes:
+            assert node.getbestblock().height == 0
+            
