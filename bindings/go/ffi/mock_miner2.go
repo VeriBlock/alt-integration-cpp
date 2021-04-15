731c1821a53165ed515d29250fc7e7aadcf0bd24
Alex Pozhylenkov <alex_pozhilenkov@adoriasoft.com>
2021-04-15 15:03:00 +0300

    Add new MockMiner version into the C interface (#689)

---
+++ bindings/go/ffi/mock_miner2.go
@@ -0,0 +1,46 @@
+package ffi
+
+// #cgo CFLAGS: -I../../../include
+// #cgo LDFLAGS: -lveriblock-pop-cpp -lstdc++ -lrocksdb -ldl -lm
+// #include <veriblock/pop/c/mock_miner2.h>
+import "C"
+import (
+	"runtime"
+)
+
+// MockMiner ...
+type MockMiner2 struct {
+	ref *C.pop_mock_miner_t
+}
+
+func NewMockMiner2() *MockMiner2 {
+	val := &MockMiner2{ref: C.pop_mock_miner_new()}
+	runtime.SetFinalizer(val, func(v *MockMiner2) {
+		v.Free()
+	})
+
+	return val
+}
+
+func (v *MockMiner2) Free() {
+	if v.ref != nil {
+		C.pop_mock_miner_free(v.ref)
+		v.ref = nil
+	}
+}
+
+func (v *MockMiner2) MineBtcBlockTip() *BtcBlock {
+	return createBtcBlock(C.pop_mock_miner_function_mineBtcBlockTip(v.ref))
+}
+
+func (v *MockMiner2) MineBtcBlock(block *BtcBlock) *BtcBlock {
+	return createBtcBlock(C.pop_mock_miner_function_mineBtcBlock(v.ref, block.ref))
+}
+
+func (v *MockMiner2) MineVbkBlockTip() *VbkBlock {
+	return createVbkBlock(C.pop_mock_miner_function_mineVbkBlockTip(v.ref))
+}
+
+func (v *MockMiner2) MineVbkBlock(block *VbkBlock) *VbkBlock {
+	return createVbkBlock(C.pop_mock_miner_function_mineVbkBlock(v.ref, block.ref))
+}
--- src/pop/c/entities/vbkblock.cpp
+++ src/pop/c/entities/vbkblock.cpp
@@ -6,6 +6,7 @@
 #include <memory>
 
 #include "vbkblock.hpp"
+#include "veriblock/pop/assert.hpp"
 
 POP_ENTITY_FREE_SIGNATURE(vbk_block) {
   if (self != nullptr) {
@@ -17,6 +18,8 @@ POP_ENTITY_FREE_SIGNATURE(vbk_block) {
 POP_ENTITY_NEW_SIGNATURE(vbk_block) { return new POP_ENTITY_NAME(vbk_block); }
 
 POP_ENTITY_GETTER_SIGNATURE(vbk_block, POP_ARRAY_NAME(u8), hash) {
+  VBK_ASSERT(self);
+
   auto hash = self->ref.getHash();
 
   POP_ARRAY_NAME(u8) res;
@@ -28,6 +31,8 @@ POP_ENTITY_GETTER_SIGNATURE(vbk_block, POP_ARRAY_NAME(u8), hash) {
 }
 
 POP_ENTITY_GETTER_SIGNATURE(vbk_block, POP_ARRAY_NAME(u8), previous_block) {
+  VBK_ASSERT(self);
+
   auto hash = self->ref.getPreviousBlock();
 
   POP_ARRAY_NAME(u8) res;
@@ -39,6 +44,8 @@ POP_ENTITY_GETTER_SIGNATURE(vbk_block, POP_ARRAY_NAME(u8), previous_block) {
 }
 
 POP_ENTITY_GETTER_SIGNATURE(vbk_block, POP_ARRAY_NAME(u8), merkle_root) {
+  VBK_ASSERT(self);
+
   auto hash = self->ref.getMerkleRoot();
 
   POP_ARRAY_NAME(u8) res;
@@ -50,6 +57,8 @@ POP_ENTITY_GETTER_SIGNATURE(vbk_block, POP_ARRAY_NAME(u8), merkle_root) {
 }
 
 POP_ENTITY_GETTER_SIGNATURE(vbk_block, POP_ARRAY_NAME(u8), previous_keystone) {
+  VBK_ASSERT(self);
+
   auto hash = self->ref.getPreviousKeystone();
 
   POP_ARRAY_NAME(u8) res;
@@ -63,6 +72,8 @@ POP_ENTITY_GETTER_SIGNATURE(vbk_block, POP_ARRAY_NAME(u8), previous_keystone) {
 POP_ENTITY_GETTER_SIGNATURE(vbk_block,
                             POP_ARRAY_NAME(u8),
                             second_previous_keystone) {
+  VBK_ASSERT(self);
+
   auto hash = self->ref.getSecondPreviousKeystone();
 
   POP_ARRAY_NAME(u8) res;
@@ -74,22 +85,32 @@ POP_ENTITY_GETTER_SIGNATURE(vbk_block,
 }
 
 POP_ENTITY_GETTER_SIGNATURE(vbk_block, int16_t, version) {
+  VBK_ASSERT(self);
+
   return self->ref.getVersion();
 }
 
 POP_ENTITY_GETTER_SIGNATURE(vbk_block, uint32_t, timestamp) {
+  VBK_ASSERT(self);
+
   return self->ref.getTimestamp();
 }
 
 POP_ENTITY_GETTER_SIGNATURE(vbk_block, int32_t, difficulty) {
+  VBK_ASSERT(self);
+
   return self->ref.getDifficulty();
 }
 
 POP_ENTITY_GETTER_SIGNATURE(vbk_block, uint64_t, nonce) {
+  VBK_ASSERT(self);
+
   return self->ref.getNonce();
 }
 
 POP_ENTITY_GETTER_SIGNATURE(vbk_block, int32_t, height) {
+  VBK_ASSERT(self);
+
   return self->ref.getHeight();
 }
 
