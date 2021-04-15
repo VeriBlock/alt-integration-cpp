daf221e836a377f3b6b7a7f893d35ea2c0254795
Mr-Leshiy <leshiy12345678@gmail.com>
2021-04-15 18:30:46 +0300

    update

---
+++ include/veriblock/pop/c/validation_state2.h
@@ -0,0 +1,23 @@
+// Copyright (c) 2019-2020 Xenios SEZC
+// https://www.veriblock.org
+// Distributed under the MIT software license, see the accompanying
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef VERIBLOCK_POP_CPP_VALIDATION_STATE2_H
+#define VERIBLOCK_POP_CPP_VALIDATION_STATE2_H
+
+#include "veriblock/pop/c/array.h"
+#include "veriblock/pop/c/entities/serde.h"
+#include "veriblock/pop/c/type_helpers.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+POP_DECLARE_ENTITY(validation_state);
+
+#ifdef __cplusplus
+}  // end of extern "C"
+#endif
+
+#endif
