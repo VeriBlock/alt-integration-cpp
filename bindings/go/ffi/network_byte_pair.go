ceb1f9b4f2aa5302e6e8ea0637595bd9103cf6f7
Alex Pozhylenkov <alex_pozhilenkov@adoriasoft.com>
2021-04-26 13:19:54 +0300

    add LevelDB storage implementation (#704)

--- bindings/go/ffi/network_byte_pair.go
+++ bindings/go/ffi/network_byte_pair.go
@@ -6,7 +6,7 @@
 package ffi
 
 // #cgo CFLAGS: -I../../../include
-// #cgo LDFLAGS: -lveriblock-pop-cpp -lstdc++ -lrocksdb -lm
+// #cgo LDFLAGS: -lveriblock-pop-cpp -lstdc++ -lm
 // #include <veriblock/pop/c/entities/network_byte_pair.h>
 import "C"
 import "runtime"
