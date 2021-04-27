962dd46d1a90d613e48b7c3a6bc81eaeceabdfc3
Shchegolev Vladimir <shchegolev_va@mail.ru>
2021-04-27 17:04:03 +0300

    c interface coin

---
+++ src/pop/c/entities/coin.hpp
@@ -0,0 +1,22 @@
+// Copyright (c) 2019-2021 Xenios SEZC
+// https://www.veriblock.org
+// Distributed under the MIT software license, see the accompanying
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef VERIBLOCK_POP_CPP_C_ENTITIES_COIN_HPP
+#define VERIBLOCK_POP_CPP_C_ENTITIES_COIN_HPP
+
+#include "default_value.hpp"
+#include "veriblock/pop/c/entities/coin.h"
+#include "veriblock/pop/entities/coin.hpp"
+
+struct __pop_coin {
+  altintegration::Coin ref;
+};
+
+namespace default_value {
+template <>
+altintegration::Coin generateDefaultValue<altintegration::Coin>();
+}
+
+#endif
