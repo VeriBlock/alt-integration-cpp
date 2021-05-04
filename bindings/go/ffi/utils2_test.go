5f23eca09929c43a1eeaece0525b087a28418559
Mr-Leshiy <leshiy12345678@gmail.com>
2021-05-04 12:54:29 +0300

    update

---
+++ bindings/go/ffi/utils2_test.go
@@ -0,0 +1,32 @@
+// Copyright (c) 2019-2021 Xenios SEZC
+// https://www.veriblock.org
+// Distributed under the MIT software license, see the accompanying
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+
+package ffi
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGeneratePublicationData(t *testing.T) {
+	assert := assert.New(t)
+
+	storage, err := NewStorage2(":inmem:")
+	assert.NoError(err)
+	defer storage.Free()
+
+	context := generateTestPopContext(t, storage)
+
+	popData := generateDefaultPopData()
+	payoutInfo := []byte{1, 2, 3, 4, 5, 6}
+	txRoot := []byte{1, 2, 3, 4, 5, 6}
+	endorsedBytes := []byte{1, 2, 3, 4, 5, 6}
+
+	publicationData, err := context.GeneratePublicationData(endorsedBytes, txRoot, payoutInfo, popData)
+
+	assert.NoError(err)
+	assert.NotNil(publicationData)
+}
