61f0abc57a2133eda7ddae9d82bb60f16ba36ba5
Mr-Leshiy <leshiy12345678@gmail.com>
2021-05-18 10:11:50 +0300

    update

--- include/veriblock/pop/blockchain/btc_chain_params.hpp
+++ include/veriblock/pop/blockchain/btc_chain_params.hpp
@@ -34,12 +34,12 @@ struct BtcChainParams {
     return mMaxFutureBlockTime;
   }
 
-  //! when finalizeBlockImpl is called, this many blocks behind final block will be
-  //! preserved in RAM. In BTC we can deallocate all blocks past final block.
+  //! when finalizeBlockImpl is called, this many blocks behind final block will
+  //! be preserved in RAM. In BTC we can deallocate all blocks past final block.
   uint32_t preserveBlocksBehindFinal() const noexcept { return 0; }
 
   //! all blocks further than this number of blocks are considered "old"
-  int32_t getOldBlocksWindow() const noexcept { return 1000; }
+  int32_t getOldBlocksWindow() const noexcept { return 12000; }
 
  protected:
   uint32_t mMaxFutureBlockTime = 2 * 60 * 60;  // 2 hours
