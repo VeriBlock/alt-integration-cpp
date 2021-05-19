5818091e7002a159fe0cd9af7903247257ca2326
Mr-Leshiy <Mr-Leshiy@users.noreply.github.com>
2021-05-19 13:24:36 +0000

    Deploying to docs from @ VeriBlock/alt-integration-cpp@52dfc5bd9fa6d38a90b0ef5192e6bac2b58a43fd 🚀

--- search/classes_0.js
+++ search/classes_0.js
@@ -1,10 +1,10 @@
 var searchData=
 [
-  ['address_427',['Address',['../structaltintegration_1_1Address.html',1,'altintegration']]],
-  ['altblock_428',['AltBlock',['../structaltintegration_1_1AltBlock.html',1,'altintegration']]],
-  ['altblocktree_429',['AltBlockTree',['../structaltintegration_1_1AltBlockTree.html',1,'altintegration']]],
-  ['altchainparams_430',['AltChainParams',['../structaltintegration_1_1AltChainParams.html',1,'altintegration']]],
-  ['arithuint256_431',['ArithUint256',['../classaltintegration_1_1ArithUint256.html',1,'altintegration']]],
-  ['atv_432',['ATV',['../structaltintegration_1_1ATV.html',1,'altintegration']]],
-  ['authenticatedcontextinfocontainer_433',['AuthenticatedContextInfoContainer',['../structaltintegration_1_1AuthenticatedContextInfoContainer.html',1,'altintegration']]]
+  ['address_428',['Address',['../structaltintegration_1_1Address.html',1,'altintegration']]],
+  ['altblock_429',['AltBlock',['../structaltintegration_1_1AltBlock.html',1,'altintegration']]],
+  ['altblocktree_430',['AltBlockTree',['../structaltintegration_1_1AltBlockTree.html',1,'altintegration']]],
+  ['altchainparams_431',['AltChainParams',['../structaltintegration_1_1AltChainParams.html',1,'altintegration']]],
+  ['arithuint256_432',['ArithUint256',['../classaltintegration_1_1ArithUint256.html',1,'altintegration']]],
+  ['atv_433',['ATV',['../structaltintegration_1_1ATV.html',1,'altintegration']]],
+  ['authenticatedcontextinfocontainer_434',['AuthenticatedContextInfoContainer',['../structaltintegration_1_1AuthenticatedContextInfoContainer.html',1,'altintegration']]]
 ];
--- search/functions_5.js
+++ search/functions_5.js
@@ -1,15 +1,15 @@
 var searchData=
 [
-  ['findblockindex_577',['findBlockIndex',['../structaltintegration_1_1BaseBlockTree.html#a7b04cff9f2bbfc3b44e43a49a61e3a5b',1,'altintegration::BaseBlockTree::findBlockIndex(const T &amp;hash)'],['../structaltintegration_1_1BaseBlockTree.html#a619da2aa9e768d96e844e78139d4e244',1,'altintegration::BaseBlockTree::findBlockIndex(const T &amp;hash) const']]],
-  ['findduplicates_578',['findDuplicates',['../namespacealtintegration.html#af5b06fe8283fa62c3c958e70bf5fd4a7',1,'altintegration']]],
-  ['findfork_579',['findFork',['../namespacealtintegration.html#aedfa430433cd4a67f1627f2e4f4000ef',1,'altintegration']]],
-  ['findvalidtips_580',['findValidTips',['../namespacealtintegration.html#afbec238293412b60961aeaf060bde22b',1,'altintegration::findValidTips(BlockIndex&lt; Block &gt; &amp;index)'],['../namespacealtintegration.html#ad65f24a687808bac04113adb107b62e8',1,'altintegration::findValidTips(const std::unordered_set&lt; BlockIndex&lt; Block &gt; * &gt; &amp;tips, BlockIndex&lt; Block &gt; &amp;index)']]],
-  ['fixedarray_581',['fixedArray',['../namespacealtintegration.html#acd30b6d85290bc936e262b61336f05fe',1,'altintegration']]],
-  ['flatscoreround_582',['flatScoreRound',['../structaltintegration_1_1PopPayoutsParams.html#a466d38bce70321042e63aa92aa529fab',1,'altintegration::PopPayoutsParams']]],
-  ['foreachnextnodepreorder_583',['forEachNextNodePreorder',['../namespacealtintegration.html#afd1423ed238d42b6348dba16fa769a96',1,'altintegration']]],
-  ['foreachnodepostorder_584',['forEachNodePostorder',['../namespacealtintegration.html#ab73d75cdeed794526d74961fcab1d6e2',1,'altintegration']]],
-  ['foreachnodepreorder_585',['forEachNodePreorder',['../namespacealtintegration.html#a869a4359297806bc6fc2821a67e7dadd',1,'altintegration']]],
-  ['fromhex_586',['fromHex',['../classaltintegration_1_1ArithUint256.html#a38e8a9d877ad66a8fdaa79fa19ea80ba',1,'altintegration::ArithUint256']]],
-  ['frompublickey_587',['fromPublicKey',['../structaltintegration_1_1Address.html#a1ee0214d5198db0fb4e9b3e946171391',1,'altintegration::Address']]],
-  ['fromstring_588',['fromString',['../structaltintegration_1_1Address.html#a832327efd93de296ee80861cd0113b15',1,'altintegration::Address']]]
+  ['findblockindex_578',['findBlockIndex',['../structaltintegration_1_1BaseBlockTree.html#a7b04cff9f2bbfc3b44e43a49a61e3a5b',1,'altintegration::BaseBlockTree::findBlockIndex(const T &amp;hash)'],['../structaltintegration_1_1BaseBlockTree.html#a619da2aa9e768d96e844e78139d4e244',1,'altintegration::BaseBlockTree::findBlockIndex(const T &amp;hash) const']]],
+  ['findduplicates_579',['findDuplicates',['../namespacealtintegration.html#af5b06fe8283fa62c3c958e70bf5fd4a7',1,'altintegration']]],
+  ['findfork_580',['findFork',['../namespacealtintegration.html#aedfa430433cd4a67f1627f2e4f4000ef',1,'altintegration']]],
+  ['findvalidtips_581',['findValidTips',['../namespacealtintegration.html#afbec238293412b60961aeaf060bde22b',1,'altintegration::findValidTips(BlockIndex&lt; Block &gt; &amp;index)'],['../namespacealtintegration.html#ad65f24a687808bac04113adb107b62e8',1,'altintegration::findValidTips(const std::unordered_set&lt; BlockIndex&lt; Block &gt; * &gt; &amp;tips, BlockIndex&lt; Block &gt; &amp;index)']]],
+  ['fixedarray_582',['fixedArray',['../namespacealtintegration.html#acd30b6d85290bc936e262b61336f05fe',1,'altintegration']]],
+  ['flatscoreround_583',['flatScoreRound',['../structaltintegration_1_1PopPayoutsParams.html#a466d38bce70321042e63aa92aa529fab',1,'altintegration::PopPayoutsParams']]],
+  ['foreachnextnodepreorder_584',['forEachNextNodePreorder',['../namespacealtintegration.html#afd1423ed238d42b6348dba16fa769a96',1,'altintegration']]],
+  ['foreachnodepostorder_585',['forEachNodePostorder',['../namespacealtintegration.html#ab73d75cdeed794526d74961fcab1d6e2',1,'altintegration']]],
+  ['foreachnodepreorder_586',['forEachNodePreorder',['../namespacealtintegration.html#a869a4359297806bc6fc2821a67e7dadd',1,'altintegration']]],
+  ['fromhex_587',['fromHex',['../classaltintegration_1_1ArithUint256.html#a38e8a9d877ad66a8fdaa79fa19ea80ba',1,'altintegration::ArithUint256']]],
+  ['frompublickey_588',['fromPublicKey',['../structaltintegration_1_1Address.html#a1ee0214d5198db0fb4e9b3e946171391',1,'altintegration::Address']]],
+  ['fromstring_589',['fromString',['../structaltintegration_1_1Address.html#a832327efd93de296ee80861cd0113b15',1,'altintegration::Address']]]
 ];
