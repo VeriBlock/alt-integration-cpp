05b0ddcaaa03cb802b6d7dc1ac91751208275078
blackyblack <blackyblack@users.noreply.github.com>
2021-05-20 11:27:29 +0000

    Deploying to docs from @ VeriBlock/alt-integration-cpp@3f02017edd0be457502a3d9fb04ccb4a64d4ac22 🚀

--- search/classes_2.js
+++ search/classes_2.js
@@ -1,11 +1,11 @@
 var searchData=
 [
-  ['chain_457',['Chain',['../structaltintegration_1_1Chain.html',1,'altintegration']]],
-  ['chain_3c_20altintegration_3a_3ablockindex_20_3e_458',['Chain&lt; altintegration::BlockIndex &gt;',['../structaltintegration_1_1Chain.html',1,'altintegration']]],
-  ['chainslice_459',['ChainSlice',['../structaltintegration_1_1ChainSlice.html',1,'altintegration']]],
-  ['checkpublicationdata_460',['CheckPublicationData',['../structaltintegration_1_1CheckPublicationData.html',1,'altintegration']]],
-  ['coin_461',['Coin',['../structaltintegration_1_1Coin.html',1,'altintegration']]],
-  ['config_462',['Config',['../structaltintegration_1_1Config.html',1,'altintegration']]],
-  ['connection_463',['Connection',['../structaltintegration_1_1signals_1_1Connection.html',1,'altintegration::signals']]],
-  ['contextinfocontainer_464',['ContextInfoContainer',['../structaltintegration_1_1ContextInfoContainer.html',1,'altintegration']]]
+  ['chain_456',['Chain',['../structaltintegration_1_1Chain.html',1,'altintegration']]],
+  ['chain_3c_20altintegration_3a_3ablockindex_20_3e_457',['Chain&lt; altintegration::BlockIndex &gt;',['../structaltintegration_1_1Chain.html',1,'altintegration']]],
+  ['chainslice_458',['ChainSlice',['../structaltintegration_1_1ChainSlice.html',1,'altintegration']]],
+  ['checkpublicationdata_459',['CheckPublicationData',['../structaltintegration_1_1CheckPublicationData.html',1,'altintegration']]],
+  ['coin_460',['Coin',['../structaltintegration_1_1Coin.html',1,'altintegration']]],
+  ['config_461',['Config',['../structaltintegration_1_1Config.html',1,'altintegration']]],
+  ['connection_462',['Connection',['../structaltintegration_1_1signals_1_1Connection.html',1,'altintegration::signals']]],
+  ['contextinfocontainer_463',['ContextInfoContainer',['../structaltintegration_1_1ContextInfoContainer.html',1,'altintegration']]]
 ];
--- search/classes_b.js
+++ search/classes_b.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['output_477',['Output',['../structaltintegration_1_1Output.html',1,'altintegration']]]
+  ['output_476',['Output',['../structaltintegration_1_1Output.html',1,'altintegration']]]
 ];
