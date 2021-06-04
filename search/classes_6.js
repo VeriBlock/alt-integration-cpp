8cbe7f8fa885c024947bc382ee3c9a43fa8b53e1
blackyblack <blackyblack@users.noreply.github.com>
2021-06-04 13:57:39 +0000

    Deploying to docs from @ VeriBlock/alt-integration-cpp@b3363b3701e123d34f71104c04cfaf06d7105720 🚀

--- search/classes_6.js
+++ search/classes_6.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['inmemstorageimpl_492',['InmemStorageImpl',['../structaltintegration_1_1adaptors_1_1InmemStorageImpl.html',1,'altintegration::adaptors']]],
-  ['inmemstorageiterator_493',['InmemStorageIterator',['../structaltintegration_1_1adaptors_1_1InmemStorageIterator.html',1,'altintegration::adaptors']]],
-  ['inmemwritebatch_494',['InmemWriteBatch',['../structaltintegration_1_1adaptors_1_1InmemWriteBatch.html',1,'altintegration::adaptors']]],
-  ['ispoppayload_495',['IsPopPayload',['../structaltintegration_1_1IsPopPayload.html',1,'altintegration']]]
+  ['inmemstorageimpl_493',['InmemStorageImpl',['../structaltintegration_1_1adaptors_1_1InmemStorageImpl.html',1,'altintegration::adaptors']]],
+  ['inmemstorageiterator_494',['InmemStorageIterator',['../structaltintegration_1_1adaptors_1_1InmemStorageIterator.html',1,'altintegration::adaptors']]],
+  ['inmemwritebatch_495',['InmemWriteBatch',['../structaltintegration_1_1adaptors_1_1InmemWriteBatch.html',1,'altintegration::adaptors']]],
+  ['ispoppayload_496',['IsPopPayload',['../structaltintegration_1_1IsPopPayload.html',1,'altintegration']]]
 ];
