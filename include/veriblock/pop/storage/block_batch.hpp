3703e08bc51ee386a41b40164aeb1955c4516bbf
alex pozhilenkov <alex_pozhilenkov@adoriasoft.com>
2021-06-18 13:42:46 +0300

    update

--- include/veriblock/pop/storage/block_batch.hpp
+++ include/veriblock/pop/storage/block_batch.hpp
@@ -27,13 +27,16 @@ struct BlockBatch {
    * @param hash
    * @param value
    */
-  virtual void writeBlock(const AltBlock::prev_hash_t& hash,
+  virtual void writeBlock(const AltBlock::hash_t& hash,
+                          const AltBlock::prev_hash_t& prev_hash,
                           const StoredBlockIndex<AltBlock>& value) = 0;
   //! @overload
-  virtual void writeBlock(const VbkBlock::prev_hash_t& hash,
+  virtual void writeBlock(const VbkBlock::hash_t& hash,
+                          const VbkBlock::prev_hash_t& prev_hash,
                           const StoredBlockIndex<VbkBlock>& value) = 0;
   //! @overload
-  virtual void writeBlock(const BtcBlock::prev_hash_t& hash,
+  virtual void writeBlock(const BtcBlock::hash_t& hash,
+                          const BtcBlock::prev_hash_t& prev_hash,
                           const StoredBlockIndex<BtcBlock>& value) = 0;
 
   /**
