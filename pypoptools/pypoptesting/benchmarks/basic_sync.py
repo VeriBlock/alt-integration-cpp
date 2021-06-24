2b79c22388a34a8c0c6f072d4b3388deaafb1ed8
blackyblack <andrey_lekar@adoriasoft.com>
2021-06-24 14:21:44 +0000

    Generate ATVs and VTBs

--- pypoptools/pypoptesting/benchmarks/basic_sync.py
+++ pypoptools/pypoptesting/benchmarks/basic_sync.py
@@ -11,10 +11,15 @@ from ..framework.sync_util import (
 class NodeBasicSyncBenchmark(PopIntegrationTestFramework):
     def set_test_params(self):
         self.num_nodes = 2
-        self.to_mine = 10
+        self.to_mine = 5000
         self.vtbs_in_payload = 10
+        self.atvs_in_payload = 10
         self.vbks_in_payload = 10
-        self.payloads_to_mine = 10
+        self.payloads_to_mine = 50
+
+        # we want to endorse previous blocks to generate ATVs
+        # make sure we have enough blocks and POP enabled
+        assert self.to_mine >= self.atvs_in_payload + 5
 
     def setup_nodes(self):
         start_all(self.nodes)
@@ -36,8 +41,8 @@ class NodeBasicSyncBenchmark(PopIntegrationTestFramework):
 
         assert self.nodes[0].getblockcount() >= self.to_mine
 
-        elapsed = time.time() - start
-        print("{} blocks mined in {:.3f} sec".format(self.to_mine, elapsed))
+        elapsed1 = time.time() - start
+        print("{} blocks mined in {:.3f} sec".format(self.to_mine, elapsed1))
 
         for e in range(self.payloads_to_mine):
             self.log.info("generate vbk blocks on node0, amount {}".format(self.vbks_in_payload))
@@ -51,24 +56,27 @@ class NodeBasicSyncBenchmark(PopIntegrationTestFramework):
                 apm.endorseVbkBlock(apm.vbkTip, apm.btcTip.getHash())
 
             last_block = self.nodes[0].getblockcount()
-            assert last_block >= 5
+            assert last_block >= self.atvs_in_payload + 5
 
-            self.log.info("endorse {} alt block".format(last_block - 5))
-            endorse_block(self.nodes[0], apm, last_block - 5)
+            self.log.info("generate atvs on node0, amount {}".format(self.atvs_in_payload))
+            for i in range(self.atvs_in_payload):
+                to_endorse = last_block - self.atvs_in_payload - 5 + i
+                endorse_block(self.nodes[0], apm, to_endorse)
 
             self.nodes[0].generate(nblocks=1)
             containing_block = self.nodes[0].getbestblock()
 
+            assert len(containing_block.containingATVs) == self.atvs_in_payload
             assert len(containing_block.containingVTBs) == self.vtbs_in_payload
-            assert len(containing_block.containingVBKs) == self.vbks_in_payload + self.vtbs_in_payload + 1
+            assert len(containing_block.containingVBKs) == self.vbks_in_payload + self.vtbs_in_payload + self.atvs_in_payload
 
         node_0_tip = self.nodes[0].getbestblock()
         node_1_tip = self.nodes[1].getbestblock()
 
         assert node_0_tip != node_1_tip
 
-        elapsed = time.time() - start - elapsed
-        print("{} payloads generated in {:.3f} sec".format(self.payloads_to_mine, elapsed))
+        elapsed2 = time.time() - start - elapsed1
+        print("{} payloads generated in {:.3f} sec".format(self.payloads_to_mine, elapsed2))
 
         self.log.info("restart all nodes")
         for node in self.nodes:
@@ -83,5 +91,5 @@ class NodeBasicSyncBenchmark(PopIntegrationTestFramework):
 
         assert self.nodes[0].getbestblock() == self.nodes[1].getbestblock()
 
-        elapsed = time.time() - start - elapsed
-        print("{} nodes synced in {:.3f} sec".format(self.num_nodes, elapsed))
+        elapsed3 = time.time() - start - elapsed2 - elapsed1
+        print("{} nodes synced in {:.3f} sec".format(self.num_nodes, elapsed3))
