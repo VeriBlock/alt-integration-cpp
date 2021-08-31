710642ad6cdee1136af3a600a348f8b329d09026
alex pozhilenkov <alex_pozhilenkov@adoriasoft.com>
2021-08-31 15:34:15 +0300

    update

--- include/veriblock/pop/blob.hpp
+++ include/veriblock/pop/blob.hpp
@@ -284,7 +284,7 @@ struct formatter<altintegration::Blob<N>> {
   auto parse(format_parse_context& ctx) -> decltype(ctx.begin()) {
     auto it = ctx.begin(), end = ctx.end();
     // Check if reached the end of the range:
-    VBK_ASSERT_MSG(it == end && *it == '}', "invalid format");
+    VBK_ASSERT_MSG(it == end || *it == '}', "invalid format");
 
     // Return an iterator past the end of the parsed range:
     return it;
