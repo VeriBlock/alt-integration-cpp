540a28c90b1cb4586927f21490e45b7f79cd6fd5
Mr-Leshiy <Mr-Leshiy@users.noreply.github.com>
2021-09-03 10:36:51 +0000

    Deploying to docs from @ VeriBlock/alt-integration-cpp@4ac9b8e5c29fc94f599875148e45447c6e04e9e7 🚀

--- search/functions_b.js
+++ search/functions_b.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['makeconnectedleafpayloadmutator_697',['makeConnectedLeafPayloadMutator',['../structaltintegration_1_1AltBlockTree.html#a8272c69b863b355ad182034984802c6d',1,'altintegration::AltBlockTree']]],
-  ['makeprevhash_698',['makePrevHash',['../structaltintegration_1_1BaseBlockTree.html#ab758c9cc0595be4b6ea8093cdad35ccb',1,'altintegration::BaseBlockTree']]],
-  ['maxaltchainfutureblocktime_699',['maxAltchainFutureBlockTime',['../structaltintegration_1_1AltChainParams.html#a1df826389de975b3c767cfce40d041f1',1,'altintegration::AltChainParams']]],
-  ['maxscorethresholdkeystone_700',['maxScoreThresholdKeystone',['../structaltintegration_1_1PopPayoutsParams.html#a07390a31c7a6067abaf8cdf1f4deac63',1,'altintegration::PopPayoutsParams']]],
-  ['maxscorethresholdnormal_701',['maxScoreThresholdNormal',['../structaltintegration_1_1PopPayoutsParams.html#af2329f9d2a0d039ed476e03e9f2b2fa3',1,'altintegration::PopPayoutsParams']]]
+  ['makeconnectedleafpayloadmutator_700',['makeConnectedLeafPayloadMutator',['../structaltintegration_1_1AltBlockTree.html#a8272c69b863b355ad182034984802c6d',1,'altintegration::AltBlockTree']]],
+  ['makeprevhash_701',['makePrevHash',['../structaltintegration_1_1BaseBlockTree.html#ab758c9cc0595be4b6ea8093cdad35ccb',1,'altintegration::BaseBlockTree']]],
+  ['maxaltchainfutureblocktime_702',['maxAltchainFutureBlockTime',['../structaltintegration_1_1AltChainParams.html#a1df826389de975b3c767cfce40d041f1',1,'altintegration::AltChainParams']]],
+  ['maxscorethresholdkeystone_703',['maxScoreThresholdKeystone',['../structaltintegration_1_1PopPayoutsParams.html#a07390a31c7a6067abaf8cdf1f4deac63',1,'altintegration::PopPayoutsParams']]],
+  ['maxscorethresholdnormal_704',['maxScoreThresholdNormal',['../structaltintegration_1_1PopPayoutsParams.html#af2329f9d2a0d039ed476e03e9f2b2fa3',1,'altintegration::PopPayoutsParams']]]
 ];
