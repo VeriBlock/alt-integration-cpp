56daaaa7c0c0e91300719d7357c3a82f1e26d871
blackyblack <blackyblack@users.noreply.github.com>
2021-09-16 12:58:21 +0000

    Deploying to docs from @ VeriBlock/alt-integration-cpp@eb79ff9b0e33c728733b8fadc69affeac3e4b592 🚀

--- navtreeindex4.js
+++ navtreeindex4.js
@@ -1,13 +1,5 @@
 var NAVTREEINDEX4 =
 {
-"structaltintegration_1_1AltBlockTree.html#a61baa26057064e2315a9ad5d9ea4a9e7":[5,0,0,5,13],
-"structaltintegration_1_1AltBlockTree.html#a62347a05a325388bc52f9bcbed5e0be0":[5,0,0,5,2],
-"structaltintegration_1_1AltBlockTree.html#a8272c69b863b355ad182034984802c6d":[5,0,0,5,25],
-"structaltintegration_1_1AltBlockTree.html#a8747831b242c792839e9acb940338c81":[5,0,0,5,7],
-"structaltintegration_1_1AltBlockTree.html#a8c3ab096334231a6d1e8dc9a80282919":[5,0,0,5,28],
-"structaltintegration_1_1AltBlockTree.html#a99ca69840eb2e8cfe725fa521381750e":[5,0,0,5,26],
-"structaltintegration_1_1AltBlockTree.html#a9c4aa4a167a177cbeb017b96572af451":[5,0,0,5,11],
-"structaltintegration_1_1AltBlockTree.html#ab3e39a5c648002f06af4cb607f508e18":[5,0,0,5,4],
 "structaltintegration_1_1AltBlockTree.html#abc288c022579a8760f7712e12578d921":[5,0,0,5,15],
 "structaltintegration_1_1AltBlockTree.html#ac093a74a6972a5e1a1ce467b2c0ef789":[5,0,0,5,29],
 "structaltintegration_1_1AltBlockTree.html#ac31836a34ff44f707091c4ea2c5cd4d1":[5,0,0,5,6],
@@ -249,5 +241,13 @@ var NAVTREEINDEX4 =
 "structaltintegration_1_1BlockIterator.html#a9f0eb8fde6e1dfe9d23cd237fa774393":[5,0,0,64,4],
 "structaltintegration_1_1BlockIterator.html#aa036df97ecb383d94ef7d95d4c2a0a58":[5,0,0,64,2],
 "structaltintegration_1_1BlockIterator.html#aaf1b99dddae3f6c9eb721bac1c50cce3":[5,0,0,64,1],
-"structaltintegration_1_1BlockIterator.html#adcb4cec77bb22dae6005cd732ecc5238":[5,0,0,64,6]
+"structaltintegration_1_1BlockIterator.html#adcb4cec77bb22dae6005cd732ecc5238":[5,0,0,64,6],
+"structaltintegration_1_1BlockIterator.html#ae01f4813a87aa7df2ee817bf3982eece":[5,0,0,64,0],
+"structaltintegration_1_1BlockIterator.html#ae489224d16febd52e4c5b8339cc9aa98":[5,0,0,64,3],
+"structaltintegration_1_1BlockReader.html":[5,0,0,65],
+"structaltintegration_1_1BlockReader.html#a1d51882198629dc2bfe95a8d24e49be6":[5,0,0,65,11],
+"structaltintegration_1_1BlockReader.html#a2d4f61258ba2bc6708b69ec5162089fc":[5,0,0,65,4],
+"structaltintegration_1_1BlockReader.html#a377c177ac69da70ac6104a79bc0e2925":[5,0,0,65,15],
+"structaltintegration_1_1BlockReader.html#a39ee1fb1fdfb14cfb67421d4a9a32cc5":[5,0,0,65,10],
+"structaltintegration_1_1BlockReader.html#a66544f9eca6ac4565b551345167e2b89":[5,0,0,65,9]
 };
--- navtreeindex8.js
+++ navtreeindex8.js
@@ -1,13 +1,5 @@
 var NAVTREEINDEX8 =
 {
-"structaltintegration_1_1adaptors_1_1BlockIteratorImpl.html":[5,0,0,0,0],
-"structaltintegration_1_1adaptors_1_1BlockIteratorImpl.html#a25e8f3fb68492da68ae2f29b6e8ac6f4":[5,0,0,0,0,4],
-"structaltintegration_1_1adaptors_1_1BlockIteratorImpl.html#a2d066dd40d688dabeb62e90880e272fa":[5,0,0,0,0,1],
-"structaltintegration_1_1adaptors_1_1BlockIteratorImpl.html#a42ebd04a609eb14d647d576146f7fe2b":[5,0,0,0,0,3],
-"structaltintegration_1_1adaptors_1_1BlockIteratorImpl.html#a5a05b03c05c8fbf81645a598c4537712":[5,0,0,0,0,6],
-"structaltintegration_1_1adaptors_1_1BlockIteratorImpl.html#a5f0ae89803b9e3e023c33b71eef5230f":[5,0,0,0,0,2],
-"structaltintegration_1_1adaptors_1_1BlockIteratorImpl.html#a6f43473699faa75b7e4b024c99ddd06b":[5,0,0,0,0,0],
-"structaltintegration_1_1adaptors_1_1BlockIteratorImpl.html#ad35dbe27e07dc10fd8dafb5252f2cbf0":[5,0,0,0,0,5],
 "structaltintegration_1_1adaptors_1_1BlockReaderImpl.html":[5,0,0,0,1],
 "structaltintegration_1_1adaptors_1_1BlockReaderImpl.html#a283736460019927cbc85a14d8196a2ac":[5,0,0,0,1,6],
 "structaltintegration_1_1adaptors_1_1BlockReaderImpl.html#a3ee02f61ecff26ef42fafab481f7c865":[5,0,0,0,1,8],
@@ -151,5 +143,13 @@ var NAVTREEINDEX8 =
 "vblake_8hpp_source.html":[6,0,1,0,0,4,4],
 "vtb_8h_source.html":[6,0,1,0,0,2,0,15],
 "vtb_8hpp_source.html":[6,0,1,0,0,5,21],
-"write__stream_8hpp_source.html":[6,0,1,0,0,49]
+"write__stream_8hpp_source.html":[6,0,1,0,0,49],
+"":[4,0,0,1],
+"":[4,0,0,7],
+"":[4,0,0,2],
+"":[4,0,0,5],
+"":[4,0,0,3],
+"":[4,0,0,0],
+"":[4,0,1],
+"":[4,0,0,6]
 };
