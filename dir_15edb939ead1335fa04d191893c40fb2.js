953c37da7c23d9df3c4ee05f0e4e3bd7f49af64e
Warchant <Warchant@users.noreply.github.com>
2021-09-21 09:54:45 +0000

    Deploying to docs from @ VeriBlock/alt-integration-cpp@068e48498d981ae16c2a8b5452a6e87d1bc303bd 🚀

--- dir_15edb939ead1335fa04d191893c40fb2.js
+++ dir_15edb939ead1335fa04d191893c40fb2.js
@@ -1,6 +1,6 @@
 var dir_15edb939ead1335fa04d191893c40fb2 =
 [
-    [ "continue_on_invalid_context.hpp", "continue__on__invalid__context_8hpp_source.html", null ],
+    [ "counting_context.hpp", "counting__context_8hpp_source.html", null ],
     [ "fork_resolution.hpp", "fork__resolution_8hpp_source.html", null ],
     [ "pop_state.hpp", "pop__state_8hpp_source.html", null ],
     [ "pop_state_machine.hpp", "pop__state__machine_8hpp_source.html", null ],
