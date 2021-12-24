63fa3651966f3746bad42b081a2dc3e1b6705413
Mr-Leshiy <leshiy12345678@gmail.com>
2021-12-24 18:22:05 +0200

    init version

---
+++ test/abfi/bitcoin/serialize_test.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2019-2021 Xenios SEZC
+// https://www.veriblock.org
+// Distributed under the MIT software license, see the accompanying
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php.
+
+#include <gtest/gtest.h>
+
+#include "veriblock/abfi/bitcoin/serialize.hpp"
+
+TEST(Serialize, BasicTest) { EXPECT_TRUE(true); }
