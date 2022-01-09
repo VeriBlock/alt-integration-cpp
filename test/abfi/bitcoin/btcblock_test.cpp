ad11196dc5bdd084c6726e2dc31c1238aed43ae6
Mr-Leshiy <leshiy12345678@gmail.com>
2022-01-09 17:45:35 +0200

    finilize blockheader serde test

--- test/abfi/bitcoin/btcblock_test.cpp
+++ test/abfi/bitcoin/btcblock_test.cpp
@@ -17,14 +17,17 @@ TEST(BlockHeader, serde_test) {
           "b6ff0b1b1680a2862a30ca44d346d9e8910d334beb48ca0c0000000000000000"),
       uint256::fromHex(
           "9d10aa52ee949386ca9385695f04ede270dda20810decd12bc9b048aaab31471"),
-      618224212,
-      818092824,
-      4271835236};
+      1415239972,
+      404472624,
+      1678286846};
   WriteStream writer;
 
   SerializeBtc(writer, header);
 
-  ASSERT_EQ(writer.HexData(), "");
+  ASSERT_EQ(writer.HexData(),
+            "02000000b6ff0b1b1680a2862a30ca44d346d9e8910d334beb48ca0c0000000000"
+            "0000009d10aa52ee949386ca9385695f04ede270dda20810decd12bc9b048aaab3"
+            "147124d95a5430c31b18fe9f0864");
 
   BlockHeader decoded{};
   ReadStream reader{writer.data()};
