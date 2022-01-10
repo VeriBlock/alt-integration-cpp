37b5a72843d41293b119571b7c8c2ee301c5ac3a
Shchegolev Vladimir <shchegolev_va@mail.ru>
2022-01-10 12:22:45 +0300

    make ATV and VTB go bindings to match the Payload interface (#917)
    * make ATV and VTB go bindings to match the Payload interface
    
    * create config in c binding
    
    * remove FIXME from atv and vtb go bindings
    
    * free config

--- src/pop/c/entities/vbkblock.cpp
+++ src/pop/c/entities/vbkblock.cpp
@@ -168,20 +168,21 @@ POP_ENTITY_SERIALIZE_TO_VBK(vbk_block) {
   return res;
 }
 
-POP_ENTITY_DESERIALIZE_FROM_VBK(vbk_block, POP_ENTITY_NAME(config) * config) {
+POP_ENTITY_DESERIALIZE_FROM_VBK(vbk_block) {
   VBK_ASSERT(state);
-  VBK_ASSERT(config);
   VBK_ASSERT(bytes.data);
-  VBK_ASSERT(config->ref);
-  VBK_ASSERT(config->ref->alt);
+
+  auto config = pop_config_new();
 
   std::vector<uint8_t> v_bytes(bytes.data, bytes.data + bytes.size);
 
   altintegration::VbkBlock out;
   if (!altintegration::DeserializeFromVbkEncoding(
           v_bytes, out, state->ref, *config->ref->alt)) {
+    pop_config_free(config);
     return nullptr;
   }
+  pop_config_free(config);
 
   auto* res = new POP_ENTITY_NAME(vbk_block);
   res->ref = std::move(out);
