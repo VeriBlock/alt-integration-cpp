87ac0d6047aee15d804d67bd3dd7627afb73deba
Bohdan Vanieiev <warchantua@gmail.com>
2022-01-31 13:34:00 +0200

    Add mingw toolchain

---
+++ cmake/toolchain/x86_64-w64-mingw32.cmake
@@ -0,0 +1,8 @@
+set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
+set(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER) # find_binary only from host
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)  # libs for target
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)  # includes for target
