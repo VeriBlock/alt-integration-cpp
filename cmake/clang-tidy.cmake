096dfbe2bcb418fe9844dc46f097c2a068964552
Bohdan Vanieiev <warchantua@gmail.com>
2022-02-01 09:43:32 +0200

    Use mold linker

--- cmake/clang-tidy.cmake
+++ cmake/clang-tidy.cmake
@@ -1,6 +1,7 @@
 function(enable_clang_tidy target)
     if (CLANG_TIDY)
         find_program(CLANG_TIDY_BIN clang-tidy REQUIRED)
+
         set(__TIDY_CMD "${CLANG_TIDY_BIN}")
 
         set_target_properties(${target} PROPERTIES
