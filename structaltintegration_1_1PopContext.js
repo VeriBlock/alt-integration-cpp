e1875f22e993407754bc1e02cfb8ca86ec1a0ba1
Mr-Leshiy <Mr-Leshiy@users.noreply.github.com>
2022-06-15 17:14:40 +0000

    Deploying to docs from @ VeriBlock/alt-integration-cpp@05d4066e67789be2b459e9796806c2226f14105a 🚀

--- structaltintegration_1_1PopContext.js
+++ structaltintegration_1_1PopContext.js
@@ -4,7 +4,7 @@ var structaltintegration_1_1PopContext =
     [ "check", "structaltintegration_1_1PopContext.html#a21c3f3db93df212c9fc53e0066debca5", null ],
     [ "check", "structaltintegration_1_1PopContext.html#ae1d9229560d0dea5fd3ac45b1fd47ec6", null ],
     [ "check", "structaltintegration_1_1PopContext.html#af713f6987db14bc4b6138f3c6ac2831c", null ],
-    [ "create", "structaltintegration_1_1PopContext.html#a3bcab636a266545057d6377bcbbb9d4c", null ],
+    [ "create", "structaltintegration_1_1PopContext.html#aa403987decee61194ecd35a8719ebf0b", null ],
     [ "generatePopData", "structaltintegration_1_1PopContext.html#ac53e8cc9c438020db9219e899b5a8bde", null ],
     [ "generatePublicationData", "structaltintegration_1_1PopContext.html#af531975fd6efdf16f6a26097f07377fd", null ],
     [ "getPopPayout", "structaltintegration_1_1PopContext.html#a02fc25257e0dc9f55845f678295a674f", null ],
